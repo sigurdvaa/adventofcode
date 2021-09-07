65ab121d4f61698cde4e8b036dc9291f0dc50995
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-07 19:47:30 +0200

    2016/05

--- 2016/05-how-about-a-nice-game-of-chess.py
+++ 2016/05-how-about-a-nice-game-of-chess.py
@@ -14,5 +14,19 @@ while True:
             break
     i += 1
 
+password = ["" for x in range(8)]
+password_len = 0
+string_ints = [str(x) for x in range(8)]
+i = 0
+while True:
+    md5 = hashlib.md5(f"{input_raw}{i}".encode()).hexdigest()
+    if md5[:5] == "00000" and md5[5] in string_ints:
+        if password[int(md5[5])] == "":
+            password[int(md5[5])] = md5[6]
+            password_len += 1
+            if password_len == 8:
+                print("Part Two: " + "".join(password))
+                break
+    i += 1
 
 
