309b6cafb14fd821d209a0fdee5eae4976933639
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-08 22:54:24 +0200

    2016/06

--- 2016/06-signals-and-noise.py
+++ 2016/06-signals-and-noise.py
@@ -1,7 +1,8 @@
 with open('06-input.txt','r') as f:
     input_lines = [x.strip() for x in f.readlines()]
 
-message = ["" for x in range(8)]
+most_message = ["" for x in range(8)]
+less_message = ["" for x in range(8)]
 for i in range(8):
     chars = {}
     for line in input_lines:
@@ -10,10 +11,14 @@ for i in range(8):
         else:
             chars[line[i]] = 1
 
-    message[i] = list(chars.keys())[0]
+    most_message[i] = list(chars.keys())[0]
+    less_message[i] = list(chars.keys())[0]
     for c in chars:
-        if chars[c] > chars[message[i]]:
-            message[i] = c
+        if chars[c] > chars[most_message[i]]:
+            most_message[i] = c
+        if chars[c] < chars[less_message[i]]:
+            less_message[i] = c
 
-print("Part One: " + "".join(message))
+print("Part One: " + "".join(most_message))
+print("Part Two: " + "".join(less_message))
 
