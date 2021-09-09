2badbd63acc0a65a2907ed6f9362a391985a945d
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-09 21:53:59 +0200

    2016/07

---
+++ 2016/07-internet-protocol-version-7.py
@@ -0,0 +1,32 @@
+import re
+
+with open('07-input.txt', 'r') as f:
+    input_ips = [x.strip() for x in f.readlines()]
+
+def abba_in_str(string):
+    for i in range(len(string)-3):
+        if string[i] == string[i+3]:
+            if string[i] != string[i+1]:
+                if string[i+1] == string[i+2]:
+                    return True
+    return False
+    
+int_tls = 0
+for ip in input_ips:
+    ip_parts = re.findall("[a-z]+", ip)
+    ip_others = ip_parts[::2]
+    ip_hypernets = ip_parts[1::2]
+
+    abba_in_hypernet = False
+    for hypernet in ip_hypernets:
+        if abba_in_str(hypernet):
+            abba_in_hypernet = True
+            break
+
+    if not abba_in_hypernet:
+        for other in ip_others:
+            if abba_in_str(other):
+                int_tls += 1
+                break
+
+print(f"Part One: {int_tls}")
