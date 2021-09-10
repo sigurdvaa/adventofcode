8344c65a1ffe38dac99549c7c56d20ab6061b22f
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-11 01:49:18 +0200

    2016/07

--- 2016/07-internet-protocol-version-7.py
+++ 2016/07-internet-protocol-version-7.py
@@ -11,6 +11,14 @@ def abba_in_str(string):
                     return True
     return False
     
+def aba_in_str(string):
+    aba = []
+    for i in range(len(string)-2):
+        if string[i] == string[i+2]:
+            if string[i] != string[i+1]:
+                aba += [string[i:i+3]]
+    return aba
+
 int_tls = 0
 for ip in input_ips:
     ip_parts = re.findall("[a-z]+", ip)
@@ -30,3 +38,25 @@ for ip in input_ips:
                 break
 
 print(f"Part One: {int_tls}")
+
+int_ssl = 0
+for ip in input_ips:
+    ip_parts = re.findall("[a-z]+", ip)
+    ip_supernets = ip_parts[::2]
+    ip_hypernets = ip_parts[1::2]
+
+    for supernet in ip_supernets:
+        break_out = False
+        supernet_aba = aba_in_str(supernet)
+        for aba in supernet_aba:
+            for hypernet in ip_hypernets:
+                if aba[1]+aba[0]+aba[1] in hypernet:
+                    int_ssl += 1
+                    break_out = True
+                    break
+            if break_out:
+                break
+        if break_out:
+            break
+
+print(f"Part Two: {int_ssl}")
