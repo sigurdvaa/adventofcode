836c6fb2fddb9b6e10b8604d7b46346acf54477c
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-13 19:20:52 +0200

    2016/09

--- 2016/09-explosives-in-cyberspace.py
+++ 2016/09-explosives-in-cyberspace.py
@@ -1,4 +1,31 @@
 with open('09-input.txt', 'r') as f:
     input_raw = f.readline().strip()
 
+decompressed = []
+input_length = len(input_raw)
+i = 0
+while i < input_length:
+    if input_raw[i] == "(":
+        
+        m = 4
+        marker_found = False
+        while not marker_found:
+            if input_raw[i+m] == ")":
+                marker_found = True
+            else:
+                m += 1
 
+        marker_split = input_raw[i+1:i+m].split("x")
+        seq_length = int(marker_split[0])
+        repeat = int(marker_split[1])
+    
+        seq = input_raw[i+m+1:i+m+1+seq_length] * repeat
+        decompressed += [seq]
+        
+        i += 1 + m + seq_length
+
+    else:
+        decompressed += [input_raw[i]]
+        i += 1
+
+print("Part One: "+str(len("".join(decompressed))))
