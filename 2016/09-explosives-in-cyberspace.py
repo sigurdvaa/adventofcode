080b0cb8aa2b6ead8963b673c6a96f07f801b9ea
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-13 21:11:57 +0200

    2016/09

--- 2016/09-explosives-in-cyberspace.py
+++ 2016/09-explosives-in-cyberspace.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10000)
+
 with open('09-input.txt', 'r') as f:
     input_raw = f.readline().strip()
 
@@ -29,3 +32,35 @@ while i < input_length:
         i += 1
 
 print("Part One: "+str(len("".join(decompressed))))
+
+def decompressed_length(string):
+    
+    decomp_len = 0
+    string_length = len(string)
+    i = 0
+    while i < string_length:
+        if string[i] == "(":
+            
+            m = 4
+            marker_found = False
+            while not marker_found:
+                if string[i+m] == ")":
+                    marker_found = True
+                else:
+                    m += 1
+
+            marker_split = string[i+1:i+m].split("x")
+            seq_length = int(marker_split[0])
+            repeat = int(marker_split[1])
+        
+            substring = string[i+m+1:i+1+m+seq_length] * repeat
+            decomp_len += decompressed_length(substring)
+
+            i += 1 + m + seq_length
+
+        else:
+            decomp_len += 1
+            i += 1
+    return decomp_len
+
+print("Part Two: "+str(decompressed_length(input_raw)))
