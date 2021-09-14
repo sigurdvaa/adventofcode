5fb2ea392fd73c08f34482c0f9ef72a49163c187
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-14 23:45:36 +0200

    2016/10

--- 2016/10-balance-bots.py
+++ 2016/10-balance-bots.py
@@ -16,53 +16,60 @@ class Bot:
         return low
 
 bots = {}
+inputs = []
 outputs = {}
-
-def bot_add(botid, value):
-    global bots
-    if not botid in bots:
-        bots[botid] = [value]
-    else:
-        bots[botid] += [value]
-        bots[botid] = sorted(bots[botid])
-    print(bots[botid])
-    if 61 in bots[botid]:
-        print(f"Part One: {botid}")
-
-def output_add(outputid):
-    global outputs
-    if not outputid in outputs:
-        outputs[outputid] = [value]
-    else:
-        outputs[outputid] += [value]
-
 for ins in input_instructions:
     split = ins.split()
     if split[0] == "value":
         botid = int(split[5])
         value = int(split[1])
-        bot_add(botid, value)
+        inputs += [{
+            "botid": botid,
+            "value": value,
+        }]
 
-for ins in input_instructions:
-    split = ins.split()
     if split[0] == "bot":
-        giver_id = int(split[1])
-        if giver_id in bots and len(bots[giver_id]) > 1:
-            low_type = split[5]
-            low_id = int(split[6])
-            high_type = split[10]
-            high_id = int(split[11])
+        bot_id = int(split[1])
+        
+        low_type = split[5]
+        low_id = int(split[6])
+        high_type = split[10]
+        high_id = int(split[11])
 
-            if low_type == "output":
-                output_add(low_id, bots[giver_id][0])
+        bots[bot_id] = {
+            "low_type": low_type,
+            "low_id": low_id,
+            "high_type": high_type,
+            "high_id": high_id,
+            "chips": [],
+        }
+    
+for value in inputs:
+    bots[value["botid"]]["chips"] += [value["value"]]
+
+    for bot in bots:
+        if len(bots[bot]["chips"]) > 1:
+            if [17, 61] in bots[bot]["chips"]:
+                print(f"Part One: {bot}")
+
+            if bots[bot]["low_type"] == "bot":
+                bots[bots[bot]["low_id"]]["chips"] += [bots[bot]["chips"][0]]
+                bots[bots[bot]["low_id"]]["chips"] = sorted(bots[bots[bot]["low_id"]]["chips"])
             else:
-                bot_add(low_id, bots[giver_id][0])
+                if bots[bot]["low_id"] in outputs:
+                    outputs["low_id"] += [bots[bot]["chips"][0]]
+                else:
+                    outputs["low_id"] = [bots[bot]["chips"][0]]
 
-            if high_type == "output":
-                output_add(high_id, bots[giver_id][-1])
+            if bots[bot]["high_type"] == "bot":
+                bots[bots[bot]["high_id"]]["chips"] += [bots[bot]["chips"][-1]]
+                bots[bots[bot]["high_id"]]["chips"] = sorted(bots[bots[bot]["high_id"]]["chips"])
             else:
-                bot_add(high_id, bots[giver_id][-1])
-
-            del bots[giver_id][-1]
-            del bots[giver_id][0]
+                if bots[bot]["high_id"] in outputs:
+                    outputs["high_id"] += [bots[bot]["chips"][-1]]
+                else:
+                    outputs["high_id"] = [bots[bot]["chips"][-1]]
+    
+            del bots[bot]["chips"][-1]
+            del bots[bot]["chips"][0]
 
