ac0416fc1c91a7366a05b4e462f5dbff29997988
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-20 23:31:00 +0200

    2016/11

--- 2016/11-radioisotope-thermoelectric-generators.py
+++ 2016/11-radioisotope-thermoelectric-generators.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 input_raw = """The first floor contains a promethium generator and a promethium-compatible microchip.
 The second floor contains a cobalt generator, a curium generator, a ruthenium generator, and a plutonium generator.
 The third floor contains a cobalt-compatible microchip, a curium-compatible microchip, a ruthenium-compatible microchip, and a plutonium-compatible microchip.
@@ -10,6 +12,22 @@ part1_items = ["e", "cog", "com", "cug", "cum", "plg", "plm", "prg", "prm", "rug
 part1_init_state = [0, 1, 2, 1, 2, 1, 2, 0, 0, 1, 2]
 part2_items = ["e", "cog", "com", "cug", "cum", "plg", "plm", "prg", "prm", "rug", "rum", "elg", "elm", "dig", "dim"]
 part2_init_state = [0, 1, 2, 1, 2, 1, 2, 0, 0, 1, 2, 0, 0, 0, 0]
+
+def unseen_state2(seen_states, state):
+    g_per_f = [0, 0, 0, 0]
+    m_per_f = [0, 0, 0, 0]
+    state_len = len(state)
+    for i in range(1, state_len, 2):
+        g_per_f[state[i]] += 1
+    for i in range(2, state_len, 2):
+        m_per_f[state[i]] += 1
+    state_arr = [state[0]] + g_per_f + m_per_f
+    state_str = ''.join(map(str, state_arr))
+    if state_str in seen_states:
+        return False
+    else:
+        seen_states.add(state_str)
+        return True
 
 def unseen_state(seen_states, state):
     state_str = "".join(map(str, state))
@@ -29,8 +47,7 @@ def valid_state(state):
                     return False
     return True
 
-def next_states(state):
-    new_states = []
+def next_states(queue, state):
     directions = []
     e = state[0]
     if e != 3:
@@ -43,45 +60,39 @@ def next_states(state):
 
         for i_1 in range(1, state_len):
             if state[i_1] == e:
-                state_1 = state.copy()
+                state_1 = state[:]
                 state_1[0] += d
                 state_1[i_1] += d
-                new_states += [state_1.copy()]
-
+                queue.append(state_1[:])
+                
                 for i_2 in range(i_1 + 1, state_len):
                     if state[i_2] == e:
-                        state_2 = state_1.copy()
+                        state_2 = state_1[:]
                         state_2[i_2] += d
-                        new_states += [state_2.copy()]
-    return new_states
+                        queue.append(state_2[:])
 
 def bfs(init_state):
-    queue = [init_state]
-    seen_states = {"".join([str(x) for x in init_state])}
+    queue = deque()
+    queue.append(init_state)
+    seen_states = set()
 
-    step = 0
+    step = -1
     solved = False
     while not solved:
         step += 1
-        new_queue = []
+        queue_len = len(queue)
         print(f"step: {step}, queue: {len(queue)}, seen: {len(seen_states)}")
-        for state in queue:
-            new_states = next_states(state)
-            for i in range(len(new_states) - 1, -1, -1):
-                if not valid_state(new_states[i]):
-                    del new_states[i]
-                elif not unseen_state(seen_states, new_states[i]):
-                    del new_states[i] 
-            new_queue += new_states
-        queue = new_queue
-        for state in queue:
-            solved = True
-            for i in state:
-                if i < 3:
-                    solved = False
+        for s in range(queue_len):
+            state = queue.popleft()
+            if valid_state(state) and unseen_state2(seen_states, state):
+                solved = True
+                for n in state:
+                    if n < 3:
+                        solved = False
+                        break
+                next_states(queue, state)
             if solved:
-             break
-
+                break
     return step
 
 print(f"Part One: {bfs(part1_init_state)}")
