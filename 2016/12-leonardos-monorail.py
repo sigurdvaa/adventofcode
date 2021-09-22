9ce81389f575923359fe9e841273119070e54326
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-22 19:23:29 +0200

    2016/12

--- 2016/12-leonardos-monorail.py
+++ 2016/12-leonardos-monorail.py
@@ -22,4 +22,33 @@ jnz d -2
 dec c
 jnz c -5"""
 
+ins = [x.split() for x in input_raw.splitlines()]
 
+def run_prog(regs, ins):
+  ip = 0
+  iend = len(ins)
+  while ip < iend:
+    if ins[ip][0] == "cpy":
+      if ins[ip][1] in regs:
+        regs[ins[ip][2]] = regs[ins[ip][1]]
+      else:
+        regs[ins[ip][2]] = int(ins[ip][1])
+    elif ins[ip][0] == "inc":
+      regs[ins[ip][1]] += 1
+    elif ins[ip][0] == "dec":
+      regs[ins[ip][1]] -= 1
+    elif ins[ip][0] == "jnz":
+      if ins[ip][1] in regs:
+        jnz = int(regs[ins[ip][1]])
+      else:
+        jnz = int(ins[ip][2])
+      if jnz != 0:
+        ip += int(ins[ip][2]) - 1
+    ip += 1
+  return regs["a"]
+
+regs = { "a": 0, "b": 0, "c": 0, "d": 0 }
+print(f"Part One: {run_prog(regs, ins)}")
+
+regs = { "a": 0, "b": 0, "c": 1, "d": 0 }
+print(f"Part Two: {run_prog(regs, ins)}")
