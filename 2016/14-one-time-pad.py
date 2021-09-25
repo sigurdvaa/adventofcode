a471cecb05ae6397f8f36fc500ab34fe9894d43f
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-09-25 16:44:57 +0200

    2016/14

--- 2016/14-one-time-pad.py
+++ 2016/14-one-time-pad.py
@@ -1,3 +1,35 @@
+from hashlib import md5
+
 input_raw = "jlmsuwbz"
+#input_raw = "abc"
 
+def valid_key(keyhashes : list, idx : int, streamlen: int):
+  key = keyhashes[idx]
+  for k in range(len(key) - 2):
+    if key[k]*3 == key[k:k+3]:
+      fivechar = key[k]*5
+      for s in range(idx + 1, idx + 1 + streamlen):
+        if fivechar in keyhashes[s]:
+          return True
+      return False
+  return False
 
+def generate_keys(salt:str, amount:int, stretch:int=1):
+  idx = 0
+  streamlen = 1000
+  keyhashes = []
+  validkeys = []
+  while len(validkeys) < amount:
+    key = salt + str(idx)
+    for s in range(stretch):
+      key = md5(key.encode("utf8")).hexdigest()
+    keyhashes += [key]
+    if len(keyhashes) > streamlen:
+      if valid_key(keyhashes, idx - streamlen, streamlen):
+        validkeys += [keyhashes[idx - streamlen]]
+    #print(f"idx: {idx}, hashes: {len(keyhashes)}, valid: {len(validkeys)}")
+    idx += 1
+  return idx - streamlen - 1
+
+print(f"Part One: {generate_keys(input_raw, 64)}")
+print(f"Part Two: {generate_keys(input_raw, 64, 2017)}")
