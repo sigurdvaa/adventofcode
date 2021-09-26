831515d422cb0b12d016e4ccac60cb4e6895208e
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-09-26 21:34:46 +0200

    2016/19

--- 2016/19-an-elephant-named-joseph.py
+++ 2016/19-an-elephant-named-joseph.py
@@ -1,3 +1,10 @@
 input_raw = "3014603"
 
+def elven_ring(amount:int):
+  elves = {}
+  for i in range(amount):
+    elves[i] = 1
+  while len(elven) > 1:
+    
 
+print(f"Part One: {elven_ring(int(input_raw))}")
