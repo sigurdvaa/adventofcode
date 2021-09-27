9b52859784e8fc71d5f8a4154cd52cbdc73a80a4
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-27 23:55:39 +0200

    2016/19

--- 2016/19-an-elephant-named-joseph.py
+++ 2016/19-an-elephant-named-joseph.py
@@ -1,40 +1,34 @@
+from collections import deque
+
 input_raw = "3014603"
-# input_raw = "5"
-
-
-def next_elf_with_present(elves: list, idx: int, amount_elves: int):
-    for i in range(idx + 1, amount_elves):
-        if elves[i]:
-            return i
-    for i in range(idx):
-        if elves[i]:
-            return i
-    return False
 
 
 def elven_ring(amount_elves: int):
-    elves = list()
-    for i in range(amount_elves):
-        elves.append(1)
+    elves = deque()
+    for i in range(1, amount_elves + 1):
+        elves.append((i, 1))
 
-    elves_left = amount_elves
-    i = 0
-    while elves_left > 1:
-        print(elves_left)
-        if elves[i]:
-            next_elf = next_elf_with_present(elves, i, amount_elves)
-            elves[i] += elves[next_elf]
-            elves[next_elf] = 0
-            i = next_elf
-            elves_left -= 1
-        else:
-            i += 1
-            if i == amount_elves:
-                i = 0
+    while len(elves) > 1:
+        first = elves.popleft()
+        second = elves.popleft()
+        elves.append((first[0], first[1] + second[1]))
 
-    for i in range(amount_elves):
-        if elves[i]:
-            return i + 1
+    return elves[0][0]
 
+
+def elven_ring2(amount_elves: int):
+    elves = deque()
+    for i in range(1, amount_elves + 1):
+        elves.append((i, 1))
+
+    while len(elves) > 1:
+        print(len(elves))
+        first = elves.popleft()
+        steal = len(elves)//2
+        elves.append((first[0], first[1] + elves[steal][1]))
+        del(elves[steal])
+
+    return elves[0][0]
 
 print(f"Part One: {elven_ring(int(input_raw))}")
+print(f"Part Two: {elven_ring2(int(input_raw))}")
