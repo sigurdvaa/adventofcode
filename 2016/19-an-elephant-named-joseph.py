af29b5d1e0cb03e91e5a58ddfa38c08f080327cb
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-09-28 19:54:49 +0200

    2016/19

--- 2016/19-an-elephant-named-joseph.py
+++ 2016/19-an-elephant-named-joseph.py
@@ -3,7 +3,7 @@ from collections import deque
 input_raw = "3014603"
 
 
-def elven_ring(amount_elves: int):
+def elven_ring_next(amount_elves: int):
     elves = deque()
     for i in range(1, amount_elves + 1):
         elves.append((i, 1))
@@ -16,19 +16,23 @@ def elven_ring(amount_elves: int):
     return elves[0][0]
 
 
-def elven_ring2(amount_elves: int):
-    elves = deque()
-    for i in range(1, amount_elves + 1):
-        elves.append((i, 1))
+def elven_ring_across(amount_elves: int):
+    elves_north = deque()
+    elves_south = deque()
+    for i in range(1, (amount_elves + 1) // 2):
+        elves_north.append((i, 1))
+    for i in range((amount_elves + 1) // 2, amount_elves + 1):
+        elves_south.append((i, 1))
 
-    while len(elves) > 1:
-        print(len(elves))
-        first = elves.popleft()
-        steal = len(elves)//2
-        elves.append((first[0], first[1] + elves[steal][1]))
-        del(elves[steal])
+    while len(elves_north) > 0:
+        first = elves_north.popleft()
+        steal = elves_south.popleft()
+        elves_south.append((first[0], first[1] + steal[1]))
+        if len(elves_south) > len(elves_north) + 1:
+            elves_north.append(elves_south.popleft())
 
-    return elves[0][0]
+    return elves_south[0][0]
 
-print(f"Part One: {elven_ring(int(input_raw))}")
-print(f"Part Two: {elven_ring2(int(input_raw))}")
+
+print(f"Part One: {elven_ring_next(int(input_raw))}")
+print(f"Part Two: {elven_ring_across(int(input_raw))}")
