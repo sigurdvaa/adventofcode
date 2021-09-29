cd2932ff53139b97f8764d7246ff26629bb056b2
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-09-29 07:54:45 +0200

    2016/19

--- 2016/19-an-elephant-named-joseph.py
+++ 2016/19-an-elephant-named-joseph.py
@@ -6,30 +6,29 @@ input_raw = "3014603"
 def elven_ring_next(amount_elves: int):
     elves = deque()
     for i in range(1, amount_elves + 1):
-        elves.append((i, 1))
+        elves.append(i)
 
     while len(elves) > 1:
-        first = elves.popleft()
-        second = elves.popleft()
-        elves.append((first[0], first[1] + second[1]))
+        elves.append(elves.popleft())
+        elves.popleft()
 
-    return elves[0][0]
+    return elves[0]
 
 
 def elven_ring_across(amount_elves: int):
     elves_right = deque()
     elves_left = deque()
     for i in range(1, (amount_elves + 1) // 2):
-        elves_right.append((i, 1))
+        elves_right.append(i)
     for i in range((amount_elves + 1) // 2, amount_elves + 1):
-        elves_left.append((i, 1))
+        elves_left.append(i)
     
     right_len = len(elves_right)
     left_len = len(elves_left)
 
     while right_len > 0:
-        first = elves_right.popleft()
-        elves_left.append((first[0], first[1] + elves_left.popleft()[1]))
+        elves_left.append(elves_right.popleft())
+        elves_left.popleft()
 
         if left_len > right_len:
             elves_right.append(elves_left.popleft())
@@ -37,7 +36,7 @@ def elven_ring_across(amount_elves: int):
         else:
             right_len -= 1
 
-    return elves_left[0][0]
+    return elves_left[0]
 
 
 print(f"Part One: {elven_ring_next(int(input_raw))}")
