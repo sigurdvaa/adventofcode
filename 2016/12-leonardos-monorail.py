97785330178a50ae48f0b6d359e3c2abf8a85e69
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-10-03 20:10:39 +0200

    2016/12

--- 2016/12-leonardos-monorail.py
+++ 2016/12-leonardos-monorail.py
@@ -29,6 +29,7 @@ def run_prog(regs, ins):
     ip = 0
     iend = len(ins)
     while ip < iend:
+        # print(ip, regs)
         if ins[ip][0] == "cpy":
             if ins[ip][1] in regs:
                 regs[ins[ip][2]] = regs[ins[ip][1]]
@@ -36,6 +37,10 @@ def run_prog(regs, ins):
                 regs[ins[ip][2]] = int(ins[ip][1])
         elif ins[ip][0] == "inc":
             if ip == 10:
+                """
+                By looking at the output of print(ip, regs) you can see which loop (ip's)
+                is taking a long time. This is to optimize ip 11, 12 and 13. a += b; b = 0; ip += 2
+                """
                 regs[ins[ip][1]] += regs["b"]
                 regs["b"] = 0
                 ip += 2
