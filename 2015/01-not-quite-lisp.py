e5fa2857bb5cb250f5f5220f836f9a919c4a1748
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-10-10 22:39:39 +0200

    2015/01

--- 2015/01-not-quite-lisp.py
+++ 2015/01-not-quite-lisp.py
@@ -1,4 +1,28 @@
 with open("01-input.txt", "r") as f:
     input_raw = f.readline().strip()
 
-print(len(input_raw))
+
+def target_floor(instructions: str):
+    floor = 0
+    for i in instructions:
+        if i == "(":
+            floor += 1
+        else:
+            floor -= 1
+    return floor
+
+
+def first_basement(instructions: str):
+    floor = 0
+    for i in range(len(instructions)):
+        if instructions[i] == "(":
+            floor += 1
+        else:
+            floor -= 1
+        if floor == -1:
+            return i+1
+    return -1
+
+
+print(f"Part One: {target_floor(input_raw)}")
+print(f"Part Two: {first_basement(input_raw)}")
