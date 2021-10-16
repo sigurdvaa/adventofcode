1ee3d7fb100e9f131df54a3a58f17daa0fda2f0d
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2021-10-16 16:28:17 +0200

    2015/04

--- 2015/04-the-ideal-stocking-stuffer.py
+++ 2015/04-the-ideal-stocking-stuffer.py
@@ -1,1 +1,18 @@
 input_raw = "iwrupvqb"
+
+
+def find_startwith_zero(secret: str, length: int = 5):
+    from hashlib import md5
+    startwithzero = "0" * length
+    i = 0
+    hashvalue = md5((secret + str(i)).encode()).hexdigest()
+    
+    while not hashvalue.startswith(startwithzero):
+        i += 1
+        hashvalue = md5((secret + str(i)).encode()).hexdigest()
+
+    return i
+
+
+print(f"Part One: {find_startwith_zero(input_raw)}")
+print(f"Part Two: {find_startwith_zero(input_raw, 6)}")
