ad72ba84bb42461cc79cbbca63c0707d44d98df8
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-11-03 11:46:58 +0100

    2015/05

--- 2015/05-doesnt-he-have-intern-elves-for-this.py
+++ 2015/05-doesnt-he-have-intern-elves-for-this.py
@@ -22,12 +22,62 @@ def is_nice(string: str):
     return True
 
 
-def count_nice(strings: list):
+def find_repeated_pairs(string: str):
+    repeated_pairs = list()
+    i = 0
+    while i < len(string) - 3:
+        match = False
+        pair = string[i] + string[i + 1]
+        for s in range(i + 2, len(string) - 1):
+            if pair == string[s] + string[s + 1]:
+                repeated_pairs.append(pair)
+                match = True
+                break
+
+        if match:
+            i += 2
+        else:
+            i += 1
+    
+    return repeated_pairs
+
+
+def find_repeated_letters(string: str):
+    repeated_letters = list()
+    i = 0
+    while i < len(string) - 2:
+        if string[i] == string[i + 2]:
+            repeated_letters.append(string[i:i + 3])
+            i += 3
+        else:
+            i += 1
+
+    return repeated_letters
+
+
+def is_nice2(string: str):
+    pairs = find_repeated_pairs(string)
+    if len(pairs) == 0:
+        return False
+
+    letters = find_repeated_letters(string)
+    if len(letters) == 0:
+        return False
+
+    return True
+
+
+def count_nice(strings: list, part: int = 1):
     count = 0
     for string in strings:
-        if is_nice(string):
-            count += 1
+        if part == 1:
+            if is_nice(string):
+                count += 1
+        elif part == 2:
+            if is_nice2(string):
+                count += 1
     return count
 
 
 print(f"Part One: {count_nice(input_raw)}")
+print(f"Part Two: {count_nice(input_raw, 2)}")
