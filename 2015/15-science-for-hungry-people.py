a56af2773943a32e87d59302c36c586e7b56a1b3
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-12-18 13:03:32 +0100

    2015/15

--- 2015/15-science-for-hungry-people.py
+++ 2015/15-science-for-hungry-people.py
@@ -5,3 +5,21 @@ Chocolate: capacity 0, durability 0, flavor -2, texture 2, calories 8"""
 
 input_test = """Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
 Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"""
+
+
+def parse_ingredients(string: str):
+    ingredients = {}
+    for line in string.splitlines():
+        split = line.split()
+        ingredients[split[0][:-1]] = {
+            "capacity": int(split[2][:-1]),
+            "durability": int(split[4][:-1]),
+            "flavor": int(split[6][:-1]),
+            "texture": int(split[8][:-1]),
+            "calories": int(split[10]),
+        }
+
+    return ingredients
+
+
+print(parse_ingredients(input_test))
