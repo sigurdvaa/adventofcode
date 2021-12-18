8678d5c4bcf21281b55c66c1b25e106a27ec7c67
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-12-18 11:24:29 +0100

    2015/15

---
+++ 2015/15-science-for-hungry-people.py
@@ -0,0 +1,7 @@
+input_raw = """Sugar: capacity 3, durability 0, flavor 0, texture -3, calories 2
+Sprinkles: capacity -3, durability 3, flavor 0, texture 0, calories 9
+Candy: capacity -1, durability 0, flavor 4, texture 0, calories 1
+Chocolate: capacity 0, durability 0, flavor -2, texture 2, calories 8"""
+
+input_test = """Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"""
