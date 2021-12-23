8f9cc9cfaa9e06515cb8b777df0f32b08d8e2b7a
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-12-23 22:46:08 +0100

    2015/21

--- 2015/21-RPG-Simulator-20XX.py
+++ 2015/21-RPG-Simulator-20XX.py
@@ -1,25 +1,105 @@
-input_raw = """Weapons:    Cost  Damage  Armor
-Dagger        8     4       0
-Shortsword   10     5       0
-Warhammer    25     6       0
-Longsword    40     7       0
-Greataxe     74     8       0
+with open("21_input.txt", "r") as f:
+    input_raw = f.read()
 
-Armor:      Cost  Damage  Armor
-Leather      13     0       1
-Chainmail    31     0       2
-Splintmail   53     0       3
-Bandedmail   75     0       4
-Platemail   102     0       5
 
-Rings:      Cost  Damage  Armor
-Damage +1    25     1       0
-Damage +2    50     2       0
-Damage +3   100     3       0
-Defense +1   20     0       1
-Defense +2   40     0       2
-Defense +3   80     0       3
+def parse_shop(strings: list) -> dict:
+    shop = {}
+    category = ""
+    for line in strings:
+        if line == "":
+            category = ""
+        elif category == "":
+            split = line.strip().split()
+            category = split[0][:-1]
+            shop[category] = []
+        else:
+            split = line.strip().split()
+            if len(split) == 4:
+                shop[category].append((int(split[1]), int(split[2]), int(split[3])))
+            else:
+                shop[category].append((int(split[2]), int(split[3]), int(split[4])))
 
-Hit Points: 104
-Damage: 8
-Armor: 1"""
+    return shop
+
+
+def parse_stats(strings: list) -> list:
+    stats = []
+    for line in strings:
+        split = line.strip().split()
+        stats.append(int(split[-1]))
+
+    return tuple(stats)
+
+
+def win_fight(me, boss) -> bool:
+    me_dmg = me[1] - boss[2]
+    if me_dmg < 1:
+        me_dmg = 1
+    me_steps_win = -(-boss[0] // me_dmg)
+
+    boss_dmg = boss[1] - me[2]
+    if boss_dmg < 1:
+        boss_dmg = 1
+    boss_steps_win = -(-me[0] // boss_dmg)
+
+    if me_steps_win <= boss_steps_win:
+        return True
+
+    return False
+
+
+def all_shopping_options(shop):
+    options = []
+    for item in shop["Weapons"]:
+        options.append([item])
+
+    next_options = options.copy()
+    for item in shop["Armor"]:
+        for option in options:
+            next_options.append(option + [item])
+    options = next_options
+
+    next_options = options.copy()
+    for item1 in shop["Rings"]:
+        for option in options:
+            next_options.append(option + [item1])
+        for item2 in shop["Rings"]:
+            if item1 != item2:
+                for option in options:
+                    next_options.append(option + [item1, item2])
+    options = next_options
+
+    return options
+
+
+def cost_of_fight(
+    boss: tuple, me: tuple, shopping_options: list, win: bool = True
+) -> int:
+    costs_win = []
+    costs_lose = []
+    for option in shopping_options:
+        cost = dmg = armor = 0
+        for item in option:
+            cost += item[0]
+            dmg += item[1]
+            armor += item[2]
+
+        if win_fight((me[0], dmg, armor), boss):
+            costs_win.append(cost)
+        else:
+            costs_lose.append(cost)
+
+    if win:
+        costs_win.sort()
+        return costs_win[0]
+    else:
+        costs_lose.sort()
+        return costs_lose[-1]
+
+
+shop = parse_shop(input_raw.splitlines()[:-10])
+boss = parse_stats(input_raw.splitlines()[23:26])
+me = parse_stats(input_raw.splitlines()[28:31])
+shopping_options = all_shopping_options(shop)
+print(f"Part One: {cost_of_fight(boss, me, shopping_options)}")
+print(f"Part Two: {cost_of_fight(boss, me, shopping_options, False)}")
