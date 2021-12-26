76fb42e0416041f12a4b7c56f7836e4fdced73f0
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2021-12-26 23:28:43 +0100

    2015/24

--- 2015/24-It-Hangs-in-the-Balance.py
+++ 2015/24-It-Hangs-in-the-Balance.py
@@ -26,3 +26,33 @@ input_raw = """1
 107
 109
 113"""
+
+
+def all_arrangements(packages: list, int_groups: int = 3) -> list:
+    arrangements = []
+    group_weight = sum(packages) // int_groups
+    groups = [[x] for x in packages if x <= group_weight]
+    find_groups = True
+    while find_groups:
+        find_groups = False
+        next_groups = []
+        for group in groups:
+            current = sum(group)
+            if current == group_weight:
+                next_groups.append(group)
+            else:
+                for weight in packages:
+                    if current + weight <= group_weight:
+                        if weight not in group:
+                            find_groups = True
+                            next_groups.append(group.copy() + [weight])
+        groups = next_groups
+    for p in sorted(groups, key=len):
+        print(p)
+
+    return arrangements
+
+
+packages = [int(x) for x in input_raw.splitlines()]
+packages = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11]
+all_arrangements(packages)
