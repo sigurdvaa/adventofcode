962e0ba489eb3d5f76ce90ab3f6deb43c946abf2
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2022-04-12 20:42:24 +0200

    2018/05

--- 2018/05-Alchemical-Reduction.py
+++ 2018/05-Alchemical-Reduction.py
@@ -8,54 +8,87 @@ def polymer_len_after_react(polymer: str) -> int:
     reacting = True
     while reacting:
         reacting = False
-        reduced_polymer = []
         i = 0
-        while i < len(polymer):
-            if i == len(polymer) - 1:
-                reduced_polymer.append(polymer[i])
-            elif (
+        polymer_len = len(polymer)
+        while i < polymer_len - 1:
+            if (
                 polymer[i].lower() == polymer[i + 1].lower()
                 and polymer[i] != polymer[i + 1]
             ):
                 reacting = True
-                i += 1
-            else:
-                reduced_polymer.append(polymer[i])
+                del polymer[i]
+                del polymer[i]
+                polymer_len -= 2
+                i -= 1
             i += 1
-
-        polymer = reduced_polymer
 
     return len(polymer)
 
 
+class Unit:
+    def __init__(self, prev, data):
+        self.prev = prev
+        self.next = None
+        self.data = data
+
+    def __repr__(self):
+        return self.data
+
+
+class Polymer:
+    def __init__(self, units):
+        self.head = None
+        for u in units:
+            if self.head == None:
+                self.head = Unit(None, u)
+                unit = self.head
+            else:
+                unit.next = Unit(unit, u)
+                unit = unit.next
+
+    def __repr__(self):
+        unit = self.head
+        units = []
+        while unit is not None:
+            units.append(unit.data)
+            unit = unit.next
+        return " <-> ".join(units)
+
+    def __len__(self):
+        if self.head == None:
+            return 0
+        else:
+            i = 0
+            unit = self.head
+            while unit is not None:
+                i += 1
+                unit = unit.next
+            return i
+
+    def react(self, unit):
+        next_unit = unit.next.next
+        if unit.prev == None:
+            self.head = next_unit
+            next_unit.prev = None
+            return next_unit
+        else:
+            next_unit.prev = unit.prev
+            unit.prev.next = next_unit
+            return unit.prev
+
+
 def polymer_len_after_react2(polymer: str) -> int:
-    from collections import namedtuple
-    Unit = namedtuple("Unit", ["type", "polarity", "reacted"])
-    polymer = [Unit(x.lower(), 0 if ord(x) > 96 else 1, False) for x in polymer]
+    polymer = Polymer(polymer)
 
-    reacting = True
-    while reacting:
-        reacting = False
-        i = 0
-        prev_i = -1
-        while i < len(polymer):
-            if polymer[i].reacted:
-                pass
-            elif prev_i == -1:
-                prev_i = i
-            if polymer[i].type == polymer[prev_i].type and polymer[i].polarity != polymer[prev_i].polarity:
-                reacting = True
-                polymer[i] = Unit(polymer[i].type, polymer[i].polarity, True)
-                polymer[prev_i] = Unit(polymer[prev_i].type, polymer[prev_i].polarity, True)
-                prev_i = -1
-            i += 1
+    unit = polymer.head
+    while unit.next is not None:
+        if unit.data.lower() == unit.next.data.lower() and unit.data != unit.next.data:
+            reacting = True
+            unit = polymer.react(unit)
+        else:
+            unit = unit.next
 
-    polymer_len = 0
-    for u in polymer:
-        if u.reacted == 0:
-            polymer_len += 1
-
-    return polymer_len
+    return len(polymer)
 
 
 def polymer_len_after_best_react(polymer: str) -> int:
@@ -66,7 +99,7 @@ def polymer_len_after_best_react(polymer: str) -> int:
     min_polymer_len = len(polymer)
     for u in units:
         new_polymer = polymer.translate({ord(u): None, ord(u.upper()): None})
-        polymer_len = polymer_len_after_react2(new_polymer)
+        polymer_len = polymer_len_after_react(new_polymer)
         if polymer_len < min_polymer_len:
             min_polymer_len = polymer_len
 
@@ -74,4 +107,4 @@ def polymer_len_after_best_react(polymer: str) -> int:
 
 
 print(f"Part One: { polymer_len_after_react2(input_raw) }")
-#print(f"Part Two: { polymer_len_after_best_react(input_raw) }")
+print(f"Part Two: { polymer_len_after_best_react(input_raw) }")
