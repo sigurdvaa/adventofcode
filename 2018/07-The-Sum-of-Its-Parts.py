6e46d28d56a60de7b1a56fbfc528101bdccc8bd3
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2022-04-17 15:49:22 +0200

    2018/07

--- 2018/07-The-Sum-of-Its-Parts.py
+++ 2018/07-The-Sum-of-Its-Parts.py
@@ -1,2 +1,20 @@
 with open("07_input.txt", "r") as f:
     input_raw = [x.strip() for x in f.readlines()]
+
+
+input_raw = """Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin."""
+
+
+def steps_order(string: str) -> str:
+    steps = {}
+    for line in string.splitlines():
+        pass
+
+
+print(f"Part One: { steps_order(input_raw) }")
