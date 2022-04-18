7aaab84fb6c016f3d1efcfddc257fbe17ee839f0
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2022-04-18 10:57:46 +0200

    2018/07

--- 2018/07-The-Sum-of-Its-Parts.py
+++ 2018/07-The-Sum-of-Its-Parts.py
@@ -1,8 +1,8 @@
 with open("07_input.txt", "r") as f:
-    input_raw = [x.strip() for x in f.readlines()]
+    input_raw = f.read()
 
 
-input_raw = """Step C must be finished before step A can begin.
+_input_raw = """Step C must be finished before step A can begin.
 Step C must be finished before step F can begin.
 Step A must be finished before step B can begin.
 Step A must be finished before step D can begin.
@@ -11,10 +11,56 @@ Step D must be finished before step E can begin.
 Step F must be finished before step E can begin."""
 
 
-def steps_order(string: str) -> str:
+def parse_steps(string: str) -> dict:
+    from collections import namedtuple
+    Step = namedtuple("Step", ["parents", "children"])
     steps = {}
     for line in string.splitlines():
-        pass
+        split = line.split()
+        parent = split[1]
+        child = split[7]
+
+        if parent not in steps:
+            steps[parent] = Step([], [child])
+        else:
+            steps[parent].children.append(child)
+
+        if child not in steps:
+            steps[child] = Step([parent], [])
+        else:
+            steps[child].parents.append(parent)
+    
+    return steps
 
 
-print(f"Part One: { steps_order(input_raw) }")
+def step_available(step: tuple, order: list):
+    for p in step.parents:
+        if p not in order:
+            return False
+    return True
+
+
+def steps_order(steps: dict) -> str:
+    next_steps = []
+    for s in steps:
+        if len(steps[s].parents) == 0:
+            next_steps.append(s)
+    next_steps.sort()
+
+    order = []
+    while len(next_steps):
+        s = next_steps.pop(0)
+        if step_available(steps[s], order):
+            order.append(s)
+            for c in steps[s].children:
+                if c not in next_steps:
+                    next_steps.append(c)
+            next_steps.sort()
+        else:
+            next_steps.append(s)
+
+    return "".join(order)
+
+
+steps = parse_steps(input_raw)
+print(f"Part One: { steps_order(steps) }")
