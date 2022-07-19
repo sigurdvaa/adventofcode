fd31dafdb30cb30ffbe38eccef608aee6c65cbf5
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2022-07-19 14:08:54 +0200

    2018/09

--- 2018/09-Marble-Mania.py
+++ 2018/09-Marble-Mania.py
@@ -7,83 +7,62 @@ class Marble:
         self.next = nxt
         self.data = data
 
-    def __repr__(self):
-        return str(self.data)
 
-
-class MarbleGame:
-    def __init__(self, players: int, marbles: int):
-        self._len = 3
-        self._player = (2 % players) + 1
-        self._marbles = marbles
+class MarbleCircle:
+    def __init__(self):
         _a = Marble(None, None, 0)
         _b = Marble(_a, None, 2)
         _c = Marble(_b, _a, 1)
         _a.next = _b
         _a.prev = _c
         _b.next = _c
-        self._head = _a
+        self.current = _b
 
     def __repr__(self):
-        curr_marble = self._head
+        curr_marble = self.current
         marbles = []
         for _ in range(len(self)):
-            marbles.append(str(curr_marble))
+            marbles.append(str(curr_marble.data))
             curr_marble = curr_marble.next
         return " <> ".join(marbles)
 
-    def __len__(self):
-        return self._len
+    def clockwise(self):
+        self.current = self.current.next.next
 
-    def react(self, unit):
-        self._len -= 2
-        if unit.prev == None:
-            self.head = unit.next.next
-            self.head.prev = None
-            return self.head
-        elif unit.next.next == None:
-            unit.prev.next = None
-            return unit.prev
-        else:
-            unit.prev.next = unit.next.next
-            unit.next.next.prev = unit.prev
-            return unit.prev
+    def counter_clockwise(self):
+        for _ in range(7):
+            self.current = self.current.prev
+
+    def insert(self, data):
+        self.current = Marble(self.current.prev, self.current, data)
+        self.current.prev.next = self.current
+        self.current.next.prev = self.current
+
+    def pop(self):
+        self.current.prev.next = self.current.next
+        self.current.next.prev = self.current.prev
+        data = self.current.data
+        self.current = self.current.next
+        return data
 
 
 def play_marble(players: int, int_marbles: int) -> int:
-    marbles: list[int] = [0, 2, 1]
-    curr_pos: int = 1
-    curr_player: int = 2
+    marbles: MarbleCircle = MarbleCircle()
     curr_marble: int = 2
+    curr_player: int = 2
     player_score: list[int] = [0] * players
     while curr_marble < int_marbles:
-        curr_pos += 2
-        if curr_pos > len(marbles):
-            curr_pos -= len(marbles)
-
         curr_player += 1
         if curr_player > players:
             curr_player = 1
 
         curr_marble += 1
         if curr_marble % 23 == 0:
-            remove_pos = curr_pos - 9
-            if remove_pos < 0:
-                remove_pos += len(marbles)
-
-            player_score[curr_player - 1] += curr_marble + marbles.pop(remove_pos)
-            curr_marble += 1
-
-            curr_pos = remove_pos
-            curr_pos += 2
-            if curr_pos > len(marbles):
-                curr_pos -= len(marbles)
-
-            curr_player += 1
-            if curr_player > players:
-                curr_player = 1
-
-        marbles.insert(curr_pos, curr_marble)
+            marbles.counter_clockwise()
+            player_score[curr_player - 1] += curr_marble + marbles.pop()
+        else:
+            marbles.clockwise()
+            marbles.insert(curr_marble)
 
     return max(player_score)
 
@@ -91,5 +70,4 @@ def play_marble(players: int, int_marbles: int) -> int:
 players = int(input_raw.split(" ")[0])
 marbles = int(input_raw.split(" ")[6])
 print(f"Part One: {play_marble(players, marbles)}")
-#print(f"Part Two: {play_marble(players, marbles*2)}")
-#print(f"Part Two: {play_marble(players, marbles*100)}")
+print(f"Part Two: {play_marble(players, marbles*100)}")
