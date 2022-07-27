b598dc15559449fee083af58ffb48eca56a33e0c
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2022-07-27 18:48:11 +0200

    2018/14

--- 2018/14-Chocolate-Charts.py
+++ 2018/14-Chocolate-Charts.py
@@ -2,33 +2,34 @@ input_raw = "110201"
 
 
 def create_recipes(target: str, backwards: bool = False) -> int:
-    recipes: list[int] = [3, 7]
+    recipes: str = "37"
     elf1: int = 0
     elf2: int = 1
+    # need to increase len by 1, as we're adding 1 or 2 recipes each loop
+    target_len: int = len(target) + 1
 
     if not backwards:
         target: int = int(target)
 
     while True:
-        new_recipe: int = recipes[elf1] + recipes[elf2]
-        if new_recipe < 10:
-            recipes.append(new_recipe)
-        else:
-            recipes.append(1)
-            recipes.append(new_recipe - 10)
+        # using ord() - 48 is faster then int()
+        # only works when converting single digit
+        recipes += str(ord(recipes[elf1]) + ord(recipes[elf2]) - 96)
 
         recipes_len: int = len(recipes)
-        elf1 = (elf1 + recipes[elf1] + 1) % recipes_len
-        elf2 = (elf2 + recipes[elf2] + 1) % recipes_len
+        elf1 = (elf1 + ord(recipes[elf1]) - 48 + 1) % recipes_len
+        elf2 = (elf2 + ord(recipes[elf2]) - 48 + 1) % recipes_len
 
         if backwards:
-            if "".join(map(str, recipes[-len(target):])) == target:
-                return len(recipes) - len(target)
+            if target in recipes[-target_len:]:
+                if target == recipes[-target_len + 1 :]:
+                    return len(recipes) - target_len + 1
+                else:
+                    return len(recipes) - target_len
         else:
             if len(recipes) >= target + 10:
-                return "".join(map(str, recipes[-10:]))
+                return recipes[-10:]
 
 
 print(f"Part One: {create_recipes(input_raw)}")
-print(f"Part Two: 2018: {create_recipes('59414', backwards=True)}")
-#print(f"Part Two: {create_recipes(input_raw, backwards=True)}")
+print(f"Part Two: {create_recipes(input_raw, backwards=True)}")
