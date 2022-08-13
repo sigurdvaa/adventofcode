47b09f4794af3b82e8a7604ad3d6741a69d01696
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2022-08-13 23:59:48 +0200

    2018/20

--- 2018/20-A-Regular-Map.py
+++ 2018/20-A-Regular-Map.py
@@ -17,16 +17,53 @@ def expand_longest_path(pattern: str) -> int:
     return len(pattern)
 
 
-input_test1 = "^WNE$"
-input_test2 = "^ENWWW(NEEE|SSE(EE|N))$"
-input_test3 = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
-input_test4 = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"
-input_test5 = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"
+def walk_paths(pattern: str, min_distance: int):
+    Point = tuple[int, int]
+    pattern = pattern[1:-1]
+    head: Point = (0, 0)
+    heads: list[Point] = []
+    rooms: set[Point] = set([head])
+    doors: set[Point] = set()
 
-print(f"Test  (3): {expand_longest_path(input_test1)}")
-print(f"Test (10): {expand_longest_path(input_test2)}")
-print(f"Test (18): {expand_longest_path(input_test3)}")
-print(f"Test (23): {expand_longest_path(input_test4)}")
-print(f"Test (31): {expand_longest_path(input_test5)}")
+    traveled_head: int = 0
+    traveled: list[int] = []
+    rooms_min_dist: set[Point] = set()
+
+    i: int = 0
+    while i < len(pattern):
+        if pattern[i] in "NSWE":
+            if pattern[i] == "N":
+                door = (head[0], head[1] - 1)
+                room = (head[0], head[1] - 2)
+            elif pattern[i] == "S":
+                door = (head[0], head[1] + 1)
+                room = (head[0], head[1] + 2)
+            elif pattern[i] == "W":
+                door = (head[0] - 1, head[1])
+                room = (head[0] - 2, head[1])
+            elif pattern[i] == "E":
+                door = (head[0] + 1, head[1])
+                room = (head[0] + 2, head[1])
+            doors.add(door)
+            rooms.add(room)
+            head = room
+            traveled_head += 1
+            if traveled_head >= min_distance:
+                rooms_min_dist.add(room)
+        elif pattern[i] == "(":
+            heads.append(head)
+            traveled.append(traveled_head)
+        elif pattern[i] == ")":
+            heads.pop()
+            traveled.pop()
+        elif pattern[i] == "|":
+            head = heads[-1]
+            traveled_head = traveled[-1]
+        else:
+            assert False, "unreachable"
+        i += 1
+    return len(rooms_min_dist)
+
 
 print(f"Part One: {expand_longest_path(input_raw)}")
+print(f"Part Two: {walk_paths(input_raw, 1000)}")
