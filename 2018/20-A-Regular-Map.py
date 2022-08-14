8c82623fed5a0d817d22d7e60f8e251d56bcd246
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2022-08-14 09:39:27 +0200

    2018/20

--- 2018/20-A-Regular-Map.py
+++ 2018/20-A-Regular-Map.py
@@ -17,53 +17,38 @@ def expand_longest_path(pattern: str) -> int:
     return len(pattern)
 
 
-def walk_paths(pattern: str, min_distance: int):
-    Point = tuple[int, int]
-    pattern = pattern[1:-1]
-    head: Point = (0, 0)
-    heads: list[Point] = []
-    rooms: set[Point] = set([head])
-    doors: set[Point] = set()
-
-    traveled_head: int = 0
-    traveled: list[int] = []
-    rooms_min_dist: set[Point] = set()
-
+def rooms_over_min_dist(pattern: str, min_dist: int):
+    Loc = tuple[int, int]
+    heads: list[tuple[int, Loc]] = []
+    loc: Loc = (0, 0)
+    rooms: set[Loc] = set([loc])
+    rooms_min_dist: int = 0
+    dist: int = 0
     i: int = 0
-    while i < len(pattern):
-        if pattern[i] in "NSWE":
-            if pattern[i] == "N":
-                door = (head[0], head[1] - 1)
-                room = (head[0], head[1] - 2)
-            elif pattern[i] == "S":
-                door = (head[0], head[1] + 1)
-                room = (head[0], head[1] + 2)
-            elif pattern[i] == "W":
-                door = (head[0] - 1, head[1])
-                room = (head[0] - 2, head[1])
-            elif pattern[i] == "E":
-                door = (head[0] + 1, head[1])
-                room = (head[0] + 2, head[1])
-            doors.add(door)
-            rooms.add(room)
-            head = room
-            traveled_head += 1
-            if traveled_head >= min_distance:
-                rooms_min_dist.add(room)
-        elif pattern[i] == "(":
-            heads.append(head)
-            traveled.append(traveled_head)
-        elif pattern[i] == ")":
+    for c in pattern:
+        if c in "NSWE":
+            if c == "N":
+                next_loc = (loc[0], loc[1] - 2)
+            elif c == "S":
+                next_loc = (loc[0], loc[1] + 2)
+            elif c == "W":
+                next_loc = (loc[0] - 2, loc[1])
+            elif c == "E":
+                next_loc = (loc[0] + 2, loc[1])
+            loc = next_loc
+            if loc not in rooms:
+                rooms.add(loc)
+                dist += 1
+                if dist >= min_dist:
+                    rooms_min_dist += 1
+        elif c == "(":
+            heads.append((dist, loc))
+        elif c == ")":
             heads.pop()
-            traveled.pop()
-        elif pattern[i] == "|":
-            head = heads[-1]
-            traveled_head = traveled[-1]
-        else:
-            assert False, "unreachable"
-        i += 1
-    return len(rooms_min_dist)
+        elif c == "|":
+            dist, loc = heads[-1]
+    return rooms_min_dist
 
 
 print(f"Part One: {expand_longest_path(input_raw)}")
-print(f"Part Two: {walk_paths(input_raw, 1000)}")
+print(f"Part Two: {rooms_over_min_dist(input_raw, 1000)}")
