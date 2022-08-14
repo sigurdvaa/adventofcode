0fec85fcd79a9f3a56339b7bb209b8f3cfe17911
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2022-08-14 10:02:40 +0200

    2018/21

--- 2018/21-Chronal-Conversion.py
+++ 2018/21-Chronal-Conversion.py
@@ -1,4 +1,55 @@
+from typing import Callable
+
+Ins = tuple[str, int, int, int]
+
 with open("21_input.txt", "r") as fp:
     input_raw: str = fp.read()
 
 
+def parse_prog(string: str) -> tuple[int, list[Ins]]:
+    lines: list[str] = string.splitlines()
+    ipx: int = int(lines[0].split()[-1])
+    ins: list[Ins] = []
+    for line in lines[1:]:
+        split = line.split()
+        ins.append((split[0], int(split[1]), int(split[2]), int(split[3])))
+    return (ipx, ins)
+
+
+def run_prog(
+    opcode: dict[str, Callable],
+    regs: list[int],
+    ipx: int,
+    prog: list[Ins],
+) -> int:
+    prog_len = len(prog)
+    while regs[ipx] < prog_len:
+        ins = prog[regs[ipx]]
+        regs[ins[3]] = opcode[ins[0]](regs, ins)
+        regs[ipx] += 1
+        print(regs)
+    return regs[0]
+
+
+opcode: dict[str, Callable] = {
+    "addr": lambda regs, ins: regs[ins[1]] + regs[ins[2]],
+    "addi": lambda regs, ins: regs[ins[1]] + ins[2],
+    "mulr": lambda regs, ins: regs[ins[1]] * regs[ins[2]],
+    "muli": lambda regs, ins: regs[ins[1]] * ins[2],
+    "banr": lambda regs, ins: regs[ins[1]] & regs[ins[2]],
+    "bani": lambda regs, ins: regs[ins[1]] & ins[2],
+    "borr": lambda regs, ins: regs[ins[1]] | regs[ins[2]],
+    "bori": lambda regs, ins: regs[ins[1]] | ins[2],
+    "setr": lambda regs, ins: regs[ins[1]],
+    "seti": lambda regs, ins: ins[1],
+    "gtir": lambda regs, ins: 1 if (ins[1] > regs[ins[2]]) else 0,
+    "gtri": lambda regs, ins: 1 if (regs[ins[1]] > ins[2]) else 0,
+    "gtrr": lambda regs, ins: 1 if (regs[ins[1]] > regs[ins[2]]) else 0,
+    "eqir": lambda regs, ins: 1 if (ins[1] == regs[ins[2]]) else 0,
+    "eqri": lambda regs, ins: 1 if (regs[ins[1]] == ins[2]) else 0,
+    "eqrr": lambda regs, ins: 1 if (regs[ins[1]] == regs[ins[2]]) else 0,
+}
+ip, prog = parse_prog(input_raw)
+
+regs: list[int] = [0, 0, 0, 0, 0, 0]
+print(f"Part One: {run_prog(opcode, regs, ip, prog)}")
