2406db26e27e6963093ce92bf3a9e4d384ce63fc
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-06-16 21:35:44 +0200

    2018/23

--- 2018/23-Experimental-Emergency-Teleportation.py
+++ 2018/23-Experimental-Emergency-Teleportation.py
@@ -2,24 +2,29 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Bot:
+class Pos:
     x: int
     y: int
     z: int
+
+    def distance(self, other: object) -> int:
+        if not isinstance(other, Pos):
+            raise NotImplementedError
+        x = abs(self.x - other.x)
+        y = abs(self.y - other.y)
+        z = abs(self.z - other.z)
+        return x + y + z
+
+
+@dataclass
+class Bot:
+    p: Pos
     r: int
 
     def __lt__(self, other: object) -> bool:
         if not isinstance(other, Bot):
             raise NotImplementedError
         return self.r < other.r
-
-    def distance(self, other: object) -> int:
-        if not isinstance(other, Bot):
-            raise NotImplementedError
-        x = abs(self.x - other.x)
-        y = abs(self.y - other.y)
-        z = abs(self.z - other.z)
-        return x + y + z
 
 
 def parse_bots(lines: list[str]) -> list[Bot]:
@@ -29,9 +34,11 @@ def parse_bots(lines: list[str]) -> list[Bot]:
         pos = split[0][5:-2].split(",")
         bots.append(
             Bot(
-                x=int(pos[0]),
-                y=int(pos[1]),
-                z=int(pos[2]),
+                p=Pos(
+                    x=int(pos[0]),
+                    y=int(pos[1]),
+                    z=int(pos[2]),
+                ),
                 r=int(split[1][2:]),
             ),
         )
@@ -41,13 +48,40 @@ def parse_bots(lines: list[str]) -> list[Bot]:
 def bots_in_range(bots: list[Bot], source: Bot) -> int:
     in_range = 0
     for bot in bots:
-        if bot.distance(source) <= source.r:
+        if bot.p.distance(source.p) <= source.r:
             in_range += 1
     return in_range
 
 
+def best_range(bots: list[Bot]) -> int:
+    max_x = max(bots, key=lambda i: i.p.x).p.x + 1
+    max_y = max(bots, key=lambda i: i.p.y).p.y + 1
+    max_z = max(bots, key=lambda i: i.p.z).p.z + 1
+    start = Pos(0, 0, 0)
+
+    best: tuple[int, int] = (0, 0)
+    for x in range(max_x):
+        for y in range(max_y):
+            for z in range(max_z):
+                p = Pos(x, y, z)
+                in_range = 0
+
+                for b in bots:
+                    d = p.distance(b.p)
+                    if d <= b.r:
+                        in_range += 1
+
+                if in_range > best[0]:
+                    best = (in_range, start.distance(p))
+                elif in_range == best[0]:
+                    if d < best[1]:
+                        best = (in_range, start.distance(p))
+
+    return best[1]
+
+
 # test
-input_example = [
+example1 = [
     "pos=<0,0,0>, r=4",
     "pos=<1,0,0>, r=1",
     "pos=<4,0,0>, r=3",
@@ -58,11 +92,24 @@ input_example = [
     "pos=<1,1,2>, r=1",
     "pos=<1,3,1>, r=1",
 ]
-bots = parse_bots(input_example)
+bots = parse_bots(example1)
 assert len(bots) == 9
 assert bots_in_range(bots, max(bots)) == 7
+example2 = [
+    "pos=<10,12,12>, r=2",
+    "pos=<12,14,12>, r=2",
+    "pos=<16,12,12>, r=4",
+    "pos=<14,14,14>, r=6",
+    "pos=<50,50,50>, r=200",
+    "pos=<10,10,10>, r=5",
+]
+bots = parse_bots(example2)
+assert best_range(bots) == 36
 
+
+# answer
 with open("23_input.txt") as fp:
     input_raw = [x.strip() for x in fp.readlines()]
 bots = parse_bots(input_raw)
 print(f"Part One: {bots_in_range(bots, max(bots))}")
+print(f"Part Two: {best_range(bots)}")
