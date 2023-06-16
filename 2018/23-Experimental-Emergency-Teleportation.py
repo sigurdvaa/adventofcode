d24679d9d1c3e96341e40406e5541471643118db
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-06-16 20:08:57 +0200

    2018/23

--- 2018/23-Experimental-Emergency-Teleportation.py
+++ 2018/23-Experimental-Emergency-Teleportation.py
@@ -1,6 +1,52 @@
-with open("23_input.txt") as fp:
-    input_raw = [x.strip() for x in fp.readlines()]
+from dataclasses import dataclass
 
+
+@dataclass
+class Bot:
+    x: int
+    y: int
+    z: int
+    r: int
+
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, Bot):
+            raise NotImplementedError
+        return self.r < other.r
+
+    def distance(self, other: object) -> int:
+        if not isinstance(other, Bot):
+            raise NotImplementedError
+        x = abs(self.x - other.x)
+        y = abs(self.y - other.y)
+        z = abs(self.z - other.z)
+        return x + y + z
+
+
+def parse_bots(lines: list[str]) -> list[Bot]:
+    bots = []
+    for line in lines:
+        split = line.split(" ")
+        pos = split[0][5:-2].split(",")
+        bots.append(
+            Bot(
+                x=int(pos[0]),
+                y=int(pos[1]),
+                z=int(pos[2]),
+                r=int(split[1][2:]),
+            ),
+        )
+    return bots
+
+
+def bots_in_range(bots: list[Bot], source: Bot) -> int:
+    in_range = 0
+    for bot in bots:
+        if bot.distance(source) <= source.r:
+            in_range += 1
+    return in_range
+
+
+# test
 input_example = [
     "pos=<0,0,0>, r=4",
     "pos=<1,0,0>, r=1",
@@ -12,3 +58,11 @@ input_example = [
     "pos=<1,1,2>, r=1",
     "pos=<1,3,1>, r=1",
 ]
+bots = parse_bots(input_example)
+assert len(bots) == 9
+assert bots_in_range(bots, max(bots)) == 7
+
+with open("23_input.txt") as fp:
+    input_raw = [x.strip() for x in fp.readlines()]
+bots = parse_bots(input_raw)
+print(f"Part One: {bots_in_range(bots, max(bots))}")
