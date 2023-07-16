9bdc15629ae6322e29465bd18028af7a9eec38b7
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-07-16 16:10:40 +0200

    2019/01

--- 2019/src/days/day1.rs
+++ 2019/src/days/day1.rs
@@ -5,26 +5,50 @@ fn fuel_required(mass: u32) -> u32 {
     (mass / 3) - 2
 }
 
+fn fuel_required_recur(mass: i32) -> i32 {
+    let fuel = (mass / 3) - 2;
+    if fuel < 0 {
+        return 0;
+    }
+    fuel + fuel_required_recur(fuel)
+}
+
 pub fn run() {
     let file_path = "inputs/day1.txt";
     let input_raw =
         fs::read_to_string(file_path).expect(format!("Error reading file '{file_path}'").as_str());
+
     let sum_fuel: u32 = input_raw
         .lines()
         .map(|x| x.parse::<u32>().unwrap())
         .map(|x| fuel_required(x))
         .sum();
     println!("Part One: {}", sum_fuel);
+
+    let sum_fuel_recur: i32 = input_raw
+        .lines()
+        .map(|x| x.parse::<i32>().unwrap())
+        .map(|x| fuel_required_recur(x))
+        .sum();
+    println!("Part Two: {}", sum_fuel_recur);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn test_input() {
+    fn test_part_one() {
         assert_eq!(fuel_required(12), 2);
         assert_eq!(fuel_required(14), 2);
         assert_eq!(fuel_required(1969), 654);
         assert_eq!(fuel_required(100756), 33583);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(fuel_required_recur(14), 2);
+        assert_eq!(fuel_required_recur(1969), 966);
+        assert_eq!(fuel_required_recur(100756), 50346);
     }
 }
