a06f78a9bad6a97aa3a9dd3a01bd555d74f3ad9f
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-07-23 22:19:06 +0200

    2019/10

--- 2019/src/days/day10.rs
+++ 2019/src/days/day10.rs
@@ -1,7 +1,66 @@
 use std::fs;
 
-fn gradient(a: (i32, i32), b: (i32, i32)) -> f32 {
-    (b.0 - a.0) as f32 / (b.1 - a.1) as f32
+fn direction(a: &(i32, i32), b: &(i32, i32)) -> (i32, i32) {
+    let dx = b.0 - a.0;
+    let dy = b.1 - a.1;
+    (
+        if dx == 0 {
+            0
+        } else {
+            if dx > 0 {
+                1
+            } else {
+                -1
+            }
+        },
+        if dy == 0 {
+            0
+        } else {
+            if dy > 0 {
+                1
+            } else {
+                -1
+            }
+        },
+    )
+}
+
+fn gradient(a: &(i32, i32), b: &(i32, i32)) -> f32 {
+    let dx = b.0 - a.0;
+    let dy = b.1 - a.1;
+    dy as f32 / dx as f32
+}
+
+fn best_detection(points: &Vec<(i32, i32)>) -> usize {
+    let mut max = 0;
+    for p1 in points {
+        let mut gradients = vec![];
+        for p2 in points {
+            if p1 == p2 {
+                continue;
+            }
+            let dg = (direction(p1, p2), gradient(p1, p2));
+            if !gradients.contains(&dg) {
+                gradients.push(dg);
+            }
+        }
+        if gradients.len() > max {
+            max = gradients.len();
+        }
+    }
+    max
+}
+
+fn parse_map(map: &str) -> Vec<(i32, i32)> {
+    let mut points = vec![];
+    for (y, line) in map.lines().enumerate() {
+        for (x, c) in line.trim().chars().enumerate() {
+            if c == '#' {
+                points.push((x as i32, y as i32));
+            }
+        }
+    }
+    points
 }
 
 pub fn run() {
@@ -10,12 +69,8 @@ pub fn run() {
     let input_raw =
         fs::read_to_string(file_path).expect(format!("Error reading file '{file_path}'").as_str());
 
-    let s = (0, 0);
-    let points = vec![(3, 1), (6, 2), (9, 3)];
-    for p in points {
-        let g = gradient(s, p);
-        println!("{}", g);
-    }
+    let points = parse_map(&input_raw);
+    println!("Part One: {}", best_detection(&points));
 }
 
 #[cfg(test)]
@@ -23,8 +78,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {}
+    fn test_part_one() {
+        let map = ".#..#\n.....\n#####\n....#\n...##";
+        let points = parse_map(&map);
+        assert_eq!(best_detection(&points), 8);
+
+        let map = "......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n\
+                   ..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####";
+        let points = parse_map(&map);
+        assert_eq!(best_detection(&points), 33);
+
+        let map = "#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n\
+                   .##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.";
+        let points = parse_map(&map);
+        assert_eq!(best_detection(&points), 35);
+
+        let map = ".#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n\
+                   ....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..";
+        let points = parse_map(&map);
+        assert_eq!(best_detection(&points), 41);
+    }
 
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        let map = ".#..##.###...#######\n##.############..##.\n.#.######.########.#\n\
+            .###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n\
+            ####################\n#.####....###.#.#.##\n##.#################\n\
+            #####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n\
+            .#####..#.######.###\n##...#.##########...\n#.##########.#######\n\
+            .####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n\
+            #.#.#.#####.####.###\n###.##.####.##.#..##";
+        let points = parse_map(&map);
+        assert_eq!(best_detection(&points), 210);
+    }
 }
