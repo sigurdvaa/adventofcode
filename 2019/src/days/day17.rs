ccf62869106d187f793aec9797437e7f0cb5c2f1
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-08-01 23:04:14 +0200

    2019/17

--- 2019/src/days/day17.rs
+++ 2019/src/days/day17.rs
@@ -1,6 +1,30 @@
 use crate::intcode::Program;
 use std::fs;
 
+fn sum_alignment_parameters(map: &str) -> usize {
+    let mut sum = 0;
+    let lines = map
+        .trim()
+        .lines()
+        .map(|line| line.trim().chars().collect())
+        .collect::<Vec<Vec<char>>>();
+    for y in 1..lines.len() - 1 {
+        for x in 1..lines[y].len() - 1 {
+            let units = vec![
+                lines[y][x],
+                lines[y][x + 1],
+                lines[y][x - 1],
+                lines[y - 1][x],
+                lines[y + 1][x],
+            ];
+            if units.iter().all(|&c| c == '#') {
+                sum += x * y;
+            }
+        }
+    }
+    sum
+}
+
 pub fn run() {
     println!("Day 17: Set and Forget");
     let file_path = "inputs/day17.txt";
@@ -9,21 +33,24 @@ pub fn run() {
 
     let mut prog = Program::new(&input_raw);
     prog.run();
-    dbg!(prog
+    let map_str = prog
         .output
         .iter()
         .map(|&x| x as u8 as char)
-        .collect::<String>()
-        .lines()
-        .collect::<Vec<_>>());
+        .collect::<String>();
+    println!("Part One: {}", sum_alignment_parameters(&map_str));
 }
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
 
     #[test]
-    fn test_part_one() {}
+    fn test_part_one() {
+        let map_str = "..#..........\n..#..........\n#######...###\n\
+        #.#...#...#.#\n#############\n..#...#...#..\n..#####...^..";
+        assert_eq!(sum_alignment_parameters(map_str), 76);
+    }
 
     #[test]
     fn test_part_two() {}
