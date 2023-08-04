1ff5120585875fb75b93bb94a403d2bbcaa14b16
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-08-04 23:00:20 +0200

    2019/17

--- 2019/src/days/day17.rs
+++ 2019/src/days/day17.rs
@@ -1,4 +1,5 @@
 use crate::intcode::Program;
+use std::collections::HashMap;
 use std::fs;
 
 fn parse_map(map: &Vec<i64>) -> Vec<Vec<char>> {
@@ -22,31 +23,24 @@ fn find_start(map: &Vec<Vec<char>>) -> Option<(usize, usize)> {
     None
 }
 
-fn trace_path(map: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
+fn trace_path(map: &Vec<Vec<char>>) -> Vec<u32> {
     let mut path = vec![];
     let mut prev = find_start(map).unwrap();
+    let mut curr = prev;
     let mut dir = 0;
     let mut turns = 0;
-    while turns < 2 {
+    while turns < 4 {
         let next = match dir {
-            0 if prev.1 > 0 => (prev.0, prev.1 - 1),
-            1 if prev.0 < map[0].len() - 1 => (prev.0 + 1, prev.1),
-            2 if prev.1 < map.len() - 1 => (prev.0, prev.1 + 1),
-            3 if prev.0 > 0 => (prev.0 - 1, prev.1),
-            _ => prev,
+            0 if curr.1 > 0 => (curr.0, curr.1 - 1),
+            1 if curr.0 < map[0].len() - 1 => (curr.0 + 1, curr.1),
+            2 if curr.1 < map.len() - 1 => (curr.0, curr.1 + 1),
+            3 if curr.0 > 0 => (curr.0 - 1, curr.1),
+            _ => curr,
         };
-        println!(
-            "dir: {}, turns: {}, prev: {:?}, next: {:?}",
-            dir, turns, prev, next
-        );
-        if next == prev {
-            dir = (dir + 1) % 4;
-            turns += 1;
-            continue;
-        }
-        if map[next.1][next.0] == '#' {
-            path.push(next);
-            prev = next;
+        if map[next.1][next.0] == '#' && next != prev && next != curr {
+            path.push(dir);
+            prev = curr;
+            curr = next;
             turns = 0;
         } else {
             dir = (dir + 1) % 4;
@@ -54,6 +48,64 @@ fn trace_path(map: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
         }
     }
     path
+}
+
+fn get_turn(next: u32, prev: u32) -> Option<char> {
+    match next {
+        _ if prev == 3 && next == 0 => Some('R'),
+        _ if next == 3 && prev == 0 => Some('L'),
+        _ if next < prev => Some('L'),
+        _ if next > prev => Some('R'),
+        _ => None,
+    }
+}
+
+fn compress_path(path: &Vec<u32>) -> Vec<(char, String)> {
+    let mut compressed = vec![];
+    let mut count = 1;
+    let mut prev = 0;
+    for i in 1..path.len() {
+        if path[i - 1] == path[i] {
+            count += 1;
+            continue;
+        }
+        compressed.push((get_turn(path[i - 1], prev).unwrap(), count.to_string()));
+        prev = path[i - 1];
+        count = 1;
+    }
+    compressed.push((
+        get_turn(*path.last().unwrap(), prev).unwrap(),
+        count.to_string(),
+    ));
+    compressed
+}
+
+fn compressed_to_string(compressed: &Vec<(char, String)>) -> String {
+    compressed
+        .iter()
+        .map(|(c, n)| format!("{c},{n}"))
+        .collect::<Vec<_>>()
+        .join(",")
+}
+
+fn count_patterns(path: &Vec<(char, String)>) -> HashMap<(char, String), u32> {
+    let mut patterns = HashMap::new();
+    for i in 0..path.len() {
+        for l in 0..i {
+            let curr = &path[l..=i];
+            if let Some(_) = patterns.get(curr) {
+                continue;
+            }
+            let mut count = 1;
+            for j in i + 1..path.len() {
+                if *curr == path[j] {
+                    count += 1;
+                }
+            }
+            patterns.insert(curr.clone(), count);
+        }
+    }
+    patterns
 }
 
 fn sum_alignment_parameters(map: &Vec<Vec<char>>) -> usize {
@@ -89,9 +141,8 @@ pub fn run() {
     println!("Part One: {}", sum_alignment_parameters(&map));
 
     let path = trace_path(&map);
-    //for l in map {
-    //    println!("{}", l.iter().collect::<String>());
-    //}
+    let _compressed = compress_path(&path);
+
     let map_str = "\
             #######...#####\n\
             #.....#...#...#\n\
@@ -110,7 +161,10 @@ pub fn run() {
             ....#####......";
     let map = parse_map(&map_str.chars().map(|x| x as i64).collect());
     let path = trace_path(&map);
-    assert_eq!(false, true);
+    let compressed = compress_path(&path);
+    let _cmp_str = compressed_to_string(&compressed);
+    let patterns = count_patterns(&compressed);
+    println!("{:?}", patterns);
 }
 
 #[cfg(test)]
@@ -151,7 +205,10 @@ mod tests {
             ....#####......";
         let map = parse_map(&map_str.chars().map(|x| x as i64).collect());
         let path = trace_path(&map);
-        assert_eq!(false, true);
-        let _strpath = "R,8,R,8,R,4,R,4,R,8,L,6,L,2,R,4,R,4,R,8,R,8,R,8,L,6,L,2";
+        let compressed = compress_path(&path);
+        assert_eq!(
+            compressed_to_string(&compressed),
+            "R,8,R,8,R,4,R,4,R,8,L,6,L,2,R,4,R,4,R,8,R,8,R,8,L,6,L,2"
+        );
     }
 }
