9dcce702fd23bac99be12b89c015ea3bc3172bff
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2023-09-06 22:12:06 +0200

    2019/20

---
+++ 2019/src/days/day20.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+pub fn run() {
+    println!("Day 20: Donut Maze");
+    let file_path = "inputs/day18.txt";
+    let input_raw =
+        fs::read_to_string(file_path).expect(format!("Error reading file '{file_path}'").as_str());
+}
+
+#[cfg(test)]
+mod tests {
+    //use super::*;
+
+    #[test]
+    fn test_part_one() {}
+
+    #[test]
+    fn test_part_two() {}
+}
