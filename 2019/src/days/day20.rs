2ec2b2ad3aca5e6dd8f73068f3928a7b8a0b1c59
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-10-15 18:03:58 +0200

    2019/20

--- 2019/src/days/day20.rs
+++ 2019/src/days/day20.rs
@@ -1,7 +1,8 @@
-use std::collections::{HashSet, VecDeque};
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::fs;
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct Portal {
     name: String,
     pos: (usize, usize),
@@ -69,91 +70,108 @@ fn find_portals(map: &Vec<Vec<char>>) -> Vec<Portal> {
     portals
 }
 
-fn shortest_path_recursive(map: &Vec<Vec<char>>) -> Option<usize> {
+fn get_portal_adj_list(map: &Vec<Vec<char>>) -> HashMap<Portal, Vec<(usize, Portal)>> {
+    let mut adj = HashMap::new();
     let portals = find_portals(map);
-    let start_portal = portals.iter().find(|portal| portal.name == "AA").unwrap();
-    let end_portal = portals.iter().find(|portal| portal.name == "ZZ").unwrap();
-    let mut seen = HashSet::new();
-    let mut queue = VecDeque::new();
-    queue.push_back(State {
-        steps: 0,
-        pos: start_portal.pos,
-        level: 0,
-        jumps: 0,
-        seen_portals: start_portal.name.clone() + "0",
-    });
+    for portal in portals.iter() {
+        let portal_adj = adj.entry(portal.clone()).or_insert(Vec::new());
 
-    while !queue.is_empty() {
-        let curr = queue.pop_front().unwrap();
-        println!("{:?}", (curr.steps, curr.jumps, portals.len(), queue.len()));
-        for dir in 0..4 {
-            let next = match dir {
-                0 if curr.pos.0 > 0 => (curr.pos.0 - 1, curr.pos.1),
-                1 if curr.pos.1 > 0 => (curr.pos.0, curr.pos.1 - 1),
-                2 if curr.pos.0 < map[curr.pos.1].len() - 1 => (curr.pos.0 + 1, curr.pos.1),
-                3 if curr.pos.1 < map.len() - 1 => (curr.pos.0, curr.pos.1 + 1),
-                _ => unreachable!(),
-            };
+        let mut seen = HashSet::new();
+        let mut queue = VecDeque::new();
+        seen.insert(portal.pos);
+        queue.push_back((0, portal.pos));
 
-            if !seen.insert((next, curr.seen_portals.clone())) {
-                continue;
-            }
+        while let Some((weight, pos)) = queue.pop_front() {
+            for dir in 0..4 {
+                let next = match dir {
+                    0 if pos.0 > 0 => (pos.0 - 1, pos.1),
+                    1 if pos.1 > 0 => (pos.0, pos.1 - 1),
+                    2 if pos.0 < map[pos.1].len() - 1 => (pos.0 + 1, pos.1),
+                    3 if pos.1 < map.len() - 1 => (pos.0, pos.1 + 1),
+                    _ => unreachable!(),
+                };
 
-            if next == end_portal.pos && curr.level == 0 {
-                return Some(curr.steps + 1);
-            }
-
-            match map[next.1][next.0] {
-                '.' => queue.push_back(State {
-                    steps: curr.steps + 1,
-                    pos: next,
-                    level: curr.level,
-                    jumps: curr.jumps,
-                    seen_portals: curr.seen_portals.clone(),
-                }),
-                c if c.is_uppercase() => {
-                    let portal = portals
-                        .iter()
-                        .find(|portal| portal.pos == curr.pos)
-                        .unwrap();
-
-                    if curr.level == 0 && portal.is_outer {
-                        continue;
-                    }
-
-                    let next_level = match portal.is_outer {
-                        true => curr.level - 1,
-                        false => curr.level + 1,
-                    };
-
-                    if curr.jumps > portals.len() * 2 {
-                        continue;
-                    }
-
-                    let mut port = portal.name.clone();
-                    port.push_str(&next_level.to_string());
-                    if curr.seen_portals[curr.seen_portals.len() - port.len()..] == port {
-                        continue;
-                    }
-
-                    let mut next_portals = curr.seen_portals.clone();
-                    next_portals.push_str(&portal.name);
-                    next_portals.push_str(&curr.level.to_string());
-
-                    if let Some(goto) = portals
-                        .iter()
-                        .find(|other| other.pos != portal.pos && other.name == portal.name)
-                    {
-                        queue.push_back(State {
-                            steps: curr.steps + 1,
-                            pos: goto.pos,
-                            level: next_level,
-                            jumps: curr.jumps + 1,
-                            seen_portals: next_portals,
-                        });
-                    }
+                if !seen.insert(next) {
+                    continue;
                 }
-                _ => (),
+
+                if map[next.1][next.0] == '.' {
+                    queue.push_back((weight + 1, next));
+                }
+
+                if let Some(other) = portals.iter().find(|p| p.pos == next) {
+                    portal_adj.push((weight + 1, other.clone()));
+                }
+            }
+        }
+    }
+    adj
+}
+
+fn shortest_path_astar(map: &Vec<Vec<char>>) -> Option<usize> {
+    let graph = get_portal_adj_list(map);
+    let mut dists: Vec<Vec<Vec<usize>>> = vec![];
+
+    None
+}
+
+fn shortest_path_levels(map: &Vec<Vec<char>>) -> Option<usize> {
+    #[derive(PartialEq, Eq, Clone, Debug)]
+    struct State {
+        weight: usize,
+        steps: usize,
+        level: usize,
+        portal: Portal,
+    }
+
+    impl Ord for State {
+        fn cmp(&self, other: &Self) -> Ordering {
+            other.weight.cmp(&self.weight)
+        }
+    }
+    impl PartialOrd for State {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(other.weight.cmp(&self.weight))
+        }
+    }
+
+    let graph = get_portal_adj_list(map);
+    let start_p = graph.keys().find(|portal| portal.name == "AA").unwrap();
+    let mut queue = BinaryHeap::from([State {
+        weight: 0,
+        steps: 0,
+        level: 0,
+        portal: start_p.clone(),
+    }]);
+
+    while let Some(state) = queue.pop() {
+        if let Some(adj) = graph.get(&state.portal) {
+            for (weight, portal) in adj {
+                if portal.name == "ZZ" && state.level == 0 {
+                    return Some(state.steps + weight);
+                }
+
+                if portal.is_outer && state.level == 0 {
+                    continue;
+                }
+
+                let next_level = match portal.is_outer {
+                    true => state.level - 1,
+                    false => state.level + 1,
+                };
+
+                if let Some(next_portal) = graph
+                    .keys()
+                    .find(|other| other.name == portal.name && other.pos != portal.pos)
+                {
+                    let next_weight = state.weight + ((next_level + 1) * weight);
+                    queue.push(State {
+                        weight: next_weight,
+                        steps: state.steps + weight + 1,
+                        level: next_level,
+                        portal: next_portal.clone(),
+                    });
+                }
             }
         }
     }
@@ -222,91 +240,7 @@ pub fn run() {
 
     let map = map_str_to_vec(_input_raw.as_str());
     println!("Part One: {}", shortest_path(&map).unwrap());
-    //println!("Part Two: {}", shortest_path_recursive(&map).unwrap());
-
-    const TESTINPUT2: &'static str = concat!(
-        "                   A               \n",
-        "                   A               \n",
-        "  #################.#############  \n",
-        "  #.#...#...................#.#.#  \n",
-        "  #.#.#.###.###.###.#########.#.#  \n",
-        "  #.#.#.......#...#.....#.#.#...#  \n",
-        "  #.#########.###.#####.#.#.###.#  \n",
-        "  #.............#.#.....#.......#  \n",
-        "  ###.###########.###.#####.#.#.#  \n",
-        "  #.....#        A   C    #.#.#.#  \n",
-        "  #######        S   P    #####.#  \n",
-        "  #.#...#                 #......VT\n",
-        "  #.#.#.#                 #.#####  \n",
-        "  #...#.#               YN....#.#  \n",
-        "  #.###.#                 #####.#  \n",
-        "DI....#.#                 #.....#  \n",
-        "  #####.#                 #.###.#  \n",
-        "ZZ......#               QG....#..AS\n",
-        "  ###.###                 #######  \n",
-        "JO..#.#.#                 #.....#  \n",
-        "  #.#.#.#                 ###.#.#  \n",
-        "  #...#..DI             BU....#..LF\n",
-        "  #####.#                 #.#####  \n",
-        "YN......#               VT..#....QG\n",
-        "  #.###.#                 #.###.#  \n",
-        "  #.#...#                 #.....#  \n",
-        "  ###.###    J L     J    #.#.###  \n",
-        "  #.....#    O F     P    #.#...#  \n",
-        "  #.###.#####.#.#####.#####.###.#  \n",
-        "  #...#.#.#...#.....#.....#.#...#  \n",
-        "  #.#####.###.###.#.#.#########.#  \n",
-        "  #...#.#.....#...#.#.#.#.....#.#  \n",
-        "  #.###.#####.###.###.#.#.#######  \n",
-        "  #.#.........#...#.............#  \n",
-        "  #########.###.###.#############  \n",
-        "           B   J   C               \n",
-        "           U   P   P               "
-    );
-    const TESTINPUT3: &'static str = concat!(
-        "             Z L X W       C                 \n",
-        "             Z P Q B       K                 \n",
-        "  ###########.#.#.#.#######.###############  \n",
-        "  #...#.......#.#.......#.#.......#.#.#...#  \n",
-        "  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  \n",
-        "  #.#...#.#.#...#.#.#...#...#...#.#.......#  \n",
-        "  #.###.#######.###.###.#.###.###.#.#######  \n",
-        "  #...#.......#.#...#...#.............#...#  \n",
-        "  #.#########.#######.#.#######.#######.###  \n",
-        "  #...#.#    F       R I       Z    #.#.#.#  \n",
-        "  #.###.#    D       E C       H    #.#.#.#  \n",
-        "  #.#...#                           #...#.#  \n",
-        "  #.###.#                           #.###.#  \n",
-        "  #.#....OA                       WB..#.#..ZH\n",
-        "  #.###.#                           #.#.#.#  \n",
-        "CJ......#                           #.....#  \n",
-        "  #######                           #######  \n",
-        "  #.#....CK                         #......IC\n",
-        "  #.###.#                           #.###.#  \n",
-        "  #.....#                           #...#.#  \n",
-        "  ###.###                           #.#.#.#  \n",
-        "XF....#.#                         RF..#.#.#  \n",
-        "  #####.#                           #######  \n",
-        "  #......CJ                       NM..#...#  \n",
-        "  ###.#.#                           #.###.#  \n",
-        "RE....#.#                           #......RF\n",
-        "  ###.###        X   X       L      #.#.#.#  \n",
-        "  #.....#        F   Q       P      #.#.#.#  \n",
-        "  ###.###########.###.#######.#########.###  \n",
-        "  #.....#...#.....#.......#...#.....#.#...#  \n",
-        "  #####.#.###.#######.#######.###.###.#.#.#  \n",
-        "  #.......#.......#.#.#.#.#...#...#...#.#.#  \n",
-        "  #####.###.#####.#.#.#.#.###.###.#.###.###  \n",
-        "  #.......#.....#.#...#...............#...#  \n",
-        "  #############.#.#.###.###################  \n",
-        "               A O F   N                     \n",
-        "               A A D   M                     "
-    );
-
-    //let map = map_str_to_vec(TESTINPUT2);
-    //assert_eq!(shortest_path_recursive(&map), None);
-    let map = map_str_to_vec(TESTINPUT3);
-    assert_eq!(shortest_path_recursive(&map), Some(396));
+    println!("Part Two: {}", shortest_path_levels(&map).unwrap());
 }
 
 #[cfg(test)]
@@ -427,12 +361,11 @@ mod tests {
     #[test]
     fn test_part_two() {
         let map = map_str_to_vec(TESTINPUT1);
-        assert_eq!(shortest_path_recursive(&map), Some(26));
-
-        let map = map_str_to_vec(TESTINPUT2);
-        assert_eq!(shortest_path_recursive(&map), None);
+        assert_eq!(shortest_path_levels(&map), Some(26));
+        assert_eq!(shortest_path_astar(&map), Some(26));
 
         let map = map_str_to_vec(TESTINPUT3);
-        assert_eq!(shortest_path_recursive(&map), Some(396));
+        assert_eq!(shortest_path_levels(&map), Some(396));
+        assert_eq!(shortest_path_astar(&map), Some(396));
     }
 }
