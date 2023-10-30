215f01732eb785735b6a79fb581d8e3f4805b38a
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-10-30 19:35:25 +0100

    2019/24

--- 2019/src/days/day24.rs
+++ 2019/src/days/day24.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::fs;
 
 fn biodiversity(cells: &Vec<Vec<char>>) -> usize {
@@ -22,15 +22,9 @@ fn biodiversity_first_repeating_layout(layout: &str) -> usize {
     seen.insert(cells.clone());
     loop {
         let next = game_of_bugs(&cells);
-        //println!();
-        //for row in next.iter() {
-        //    println!("{:?}", row.iter().collect::<String>())
-        //}
-
         if !seen.insert(next.clone()) {
             return biodiversity(&next);
         }
-
         cells = next;
     }
 }
@@ -71,7 +65,6 @@ fn game_of_bugs(cells: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     for (y, row) in cells.iter().enumerate() {
         for (x, cell) in row.iter().enumerate() {
             let adjacent = count_adjacent_bugs(&cells, x, y);
-            //println!("x: {x}, y: {y}, adjacent: {adjacent}");
             match cell {
                 '.' => {
                     if adjacent > 0 && adjacent < 3 {
@@ -91,6 +84,49 @@ fn game_of_bugs(cells: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     next
 }
 
+fn game_of_bugs_recusive(levels: &Vec<Vec<Vec<char>>>, idx: usize) -> Vec<Vec<char>> {
+    let mut next = levels[idx].clone();
+
+    for (y, row) in cells.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            let adjacent = count_adjacent_bugs(&cells, x, y);
+            match cell {
+                '.' => {
+                    if adjacent > 0 && adjacent < 3 {
+                        next[y][x] = '#'
+                    }
+                }
+                '#' => {
+                    if adjacent != 1 {
+                        next[y][x] = '.'
+                    };
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    next
+}
+
+fn bugs_after_minutes(layout: &str, minutes: usize) -> usize {
+    let mut cells = layout
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let ref_cells = vec![vec!['.'; cells[0].len()]; cells.len()];
+    let mut levels = VecDeque::from([cells]);
+    for _ in 0..minutes {
+        for (i, level) in levels.iter_mut().enumerate() {
+            let next = game_of_bugs_recursive(&levels, i);
+            level = next;
+        }
+        levels.push_front(ref_cells.clone());
+        levels.push_back(ref_cells.clone());
+    }
+    0
+}
+
 pub fn run() {
     println!("Day 24: Planet of Discord");
     let file_path = "inputs/day24.txt";
@@ -101,7 +137,7 @@ pub fn run() {
         "Part One: {}",
         biodiversity_first_repeating_layout(&input_raw)
     );
-    println!("Part Two: {}", "TODO");
+    println!("Part Two: {}", bugs_after_minutes(&input_raw, 200));
 }
 
 #[cfg(test)]
@@ -113,6 +149,10 @@ mod tests {
         let input_test = "....#\n#..#.\n#..##\n..#..\n#....";
         assert_eq!(biodiversity_first_repeating_layout(&input_test), 2129920);
     }
+
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        let input_test = "....#\n#..#.\n#..##\n..#..\n#....";
+        assert_eq!(bugs_after_minutes(&input_test, 10), 99);
+    }
 }
