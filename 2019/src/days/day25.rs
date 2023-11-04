d78845feec72fa2471cb9fbf22b3a4017fb82927
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-11-04 19:31:10 +0100

    2019/25

--- 2019/src/days/day25.rs
+++ 2019/src/days/day25.rs
@@ -2,6 +2,7 @@ use crate::intcode::{ExitCode, Program};
 use std::collections::{HashSet, VecDeque};
 use std::fs;
 
+#[allow(dead_code)]
 enum Command {
     Movement(String),
     Take(String),
@@ -23,8 +24,10 @@ impl Command {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Location {
     name: String,
+    desc: String,
     doors: Vec<String>,
     items: Vec<String>,
+    msg: String,
 }
 
 fn parse_droid_output(output: &mut Vec<i64>) -> Location {
@@ -32,12 +35,17 @@ fn parse_droid_output(output: &mut Vec<i64>) -> Location {
     output.clear();
     let mut lines = output_str.lines();
     let mut name = String::new();
+    let mut desc = String::new();
     let mut doors = vec![];
     let mut items = vec![];
+    let mut msg = String::new();
 
     while let Some(line) = lines.next() {
         if line.starts_with("==") {
             name = line.into();
+            if let Some(d) = lines.next() {
+                desc = d.into();
+            }
             continue;
         }
 
@@ -58,40 +66,110 @@ fn parse_droid_output(output: &mut Vec<i64>) -> Location {
                 items.push(item[2..].into());
             }
         }
+
+        msg = line.into();
     }
 
-    Location { name, doors, items }
+    Location {
+        name,
+        desc,
+        doors,
+        items,
+        msg,
+    }
 }
 
 fn find_airlock_password(prog: &Program) -> Option<String> {
-    let mut queue = VecDeque::from([prog.clone()]);
+    let mut queue = VecDeque::from([(prog.clone(), vec![])]);
     let mut seen = HashSet::new();
+    let skip_items = vec![
+        "photons".to_string(),
+        "infinite loop".to_string(),
+        "molten lava".to_string(),
+        "escape pod".to_string(),
+        "spool of cat6".to_string(),
+    ];
 
-    while let Some(mut curr) = queue.pop_front() {
-        let exitcode = curr.run();
+    while let Some((mut curr_prog, curr_items)) = queue.pop_front() {
+        let exitcode = curr_prog.run();
+        let curr_loc = parse_droid_output(&mut curr_prog.output);
+        if curr_loc.name == "== Pressure-Sensitive Floor ==" {
+            if !curr_loc.msg.contains("Alert!") {
+                return Some(curr_loc.msg.clone());
+            }
+        }
 
         if exitcode == ExitCode::Halted {
             continue;
         }
 
-        let curr_loc = parse_droid_output(&mut curr.output);
-        if !seen.insert(curr_loc.clone()) {
+        if !seen.insert((curr_loc.clone(), curr_items.clone())) {
             continue;
         }
 
         for door in curr_loc.doors.iter() {
-            let mut next_prog = curr.clone();
-            next_prog.input.extend(
+            let mut prog_next_loc = curr_prog.clone();
+            prog_next_loc.input.extend(
                 Command::Movement(door.into())
                     .value()
                     .chars()
                     .map(|c| c as i64)
                     .rev(),
             );
-            queue.push_back(next_prog);
+            queue.push_back((prog_next_loc.clone(), curr_items.clone()));
+
+            for item in curr_loc.items.iter() {
+                if skip_items.contains(item) {
+                    continue;
+                }
+
+                let mut prog_next_item = prog_next_loc.clone();
+                let mut next_items = curr_items.clone();
+                next_items.push(item.to_string());
+                next_items.sort();
+                prog_next_item.input.extend(
+                    Command::Take(item.into())
+                        .value()
+                        .chars()
+                        .map(|c| c as i64)
+                        .rev(),
+                );
+                queue.push_back((prog_next_item, next_items));
+            }
         }
     }
     None
+}
+
+#[allow(dead_code)]
+fn control_droid(mut prog: Program) {
+    use std::io::{self, Write};
+    while prog.run() != ExitCode::Halted {
+        println!(
+            "{}",
+            prog.output
+                .iter()
+                .map(|&i| i as u8 as char)
+                .collect::<String>()
+        );
+        prog.output.clear();
+
+        print!("cmd: ");
+        io::stdout().flush().unwrap();
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Unable to read command");
+
+        prog.input.extend(buffer.chars().map(|c| c as i64).rev());
+    }
+    println!(
+        "\nHALTED:\n{}",
+        prog.output
+            .iter()
+            .map(|&i| i as u8 as char)
+            .collect::<String>()
+    );
 }
 
 pub fn run() {
@@ -102,7 +180,8 @@ pub fn run() {
 
     let prog = Program::new(&input_raw);
     println!("Part One: {}", find_airlock_password(&prog).unwrap());
-    println!("Part Two: {}", "TODO");
+    println!("Part Two: {}", "n/a");
+    //control_droid(prog);
 }
 
 #[cfg(test)]
