d5d11dbeb3a1198a09de123779043458ee3f9a9d
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-11-04 16:17:56 +0100

    2019/25

--- 2019/src/days/day25.rs
+++ 2019/src/days/day25.rs
@@ -1,4 +1,5 @@
 use crate::intcode::{ExitCode, Program};
+use std::collections::{HashSet, VecDeque};
 use std::fs;
 
 enum Command {
@@ -19,30 +20,76 @@ impl Command {
     }
 }
 
-fn find_password(mut prog: Program) -> Option<String> {
-    // explore to pick up all items that doesn't kill you and go to checkpoint
-    // bfs dropping items to enter next root
-    let input = vec![
-        Command::Movement("north".into()).value(),
-        Command::Take("photons".into()).value(),
-        Command::Inventory.value(),
-    ];
-    for i in input {
-        prog.input.extend(i.chars().map(|c| c as i64).rev());
-        let exitcode = prog.run();
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Location {
+    name: String,
+    doors: Vec<String>,
+    items: Vec<String>,
+}
 
-        if exitcode == ExitCode::Halted {
-            return Some("died".into());
+fn parse_droid_output(output: &mut Vec<i64>) -> Location {
+    let output_str = output.iter().map(|&i| i as u8 as char).collect::<String>();
+    output.clear();
+    let mut lines = output_str.lines();
+    let mut name = String::new();
+    let mut doors = vec![];
+    let mut items = vec![];
+
+    while let Some(line) = lines.next() {
+        if line.starts_with("==") {
+            name = line.into();
+            continue;
         }
 
-        println!(
-            "{exitcode:?}\n{}",
-            prog.output
-                .iter()
-                .map(|&i| i as u8 as char)
-                .collect::<String>()
-        );
-        prog.output.clear();
+        if line.starts_with("Doors here lead:") {
+            while let Some(door) = lines.next() {
+                if !door.starts_with("- ") {
+                    break;
+                }
+                doors.push(door[2..].into());
+            }
+        }
+
+        if line.starts_with("Items here:") {
+            while let Some(item) = lines.next() {
+                if !item.starts_with("- ") {
+                    break;
+                }
+                items.push(item[2..].into());
+            }
+        }
+    }
+
+    Location { name, doors, items }
+}
+
+fn find_airlock_password(prog: &Program) -> Option<String> {
+    let mut queue = VecDeque::from([prog.clone()]);
+    let mut seen = HashSet::new();
+
+    while let Some(mut curr) = queue.pop_front() {
+        let exitcode = curr.run();
+
+        if exitcode == ExitCode::Halted {
+            continue;
+        }
+
+        let curr_loc = parse_droid_output(&mut curr.output);
+        if !seen.insert(curr_loc.clone()) {
+            continue;
+        }
+
+        for door in curr_loc.doors.iter() {
+            let mut next_prog = curr.clone();
+            next_prog.input.extend(
+                Command::Movement(door.into())
+                    .value()
+                    .chars()
+                    .map(|c| c as i64)
+                    .rev(),
+            );
+            queue.push_back(next_prog);
+        }
     }
     None
 }
@@ -54,7 +101,7 @@ pub fn run() {
         fs::read_to_string(file_path).expect(format!("Error reading file '{file_path}'").as_str());
 
     let prog = Program::new(&input_raw);
-    println!("Part One: {}", find_password(prog.clone()).unwrap());
+    println!("Part One: {}", find_airlock_password(&prog).unwrap());
     println!("Part Two: {}", "TODO");
 }
 
