ad46059a67172d01a8eb6d3fea46937c4e0b6820
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2023-11-09 22:30:53 +0100

    2020/02

--- 2020/src/days/day02.rs
+++ 2020/src/days/day02.rs
@@ -3,9 +3,24 @@ use std::fs;
 pub fn run() {
     println!("Day 2: Password Philosophy");
     let file_path = "inputs/day02.txt";
-    let _input_raw =
+    let input_raw =
         fs::read_to_string(file_path).expect(format!("Error reading file '{file_path}'").as_str());
 
+    let passwords = input_raw
+        .lines()
+        .filter(|&line| line != "")
+        .map(|line| {
+            let mut split = line.split(" ");
+            let mut range = split.next().unwrap().split("-");
+            let start = range.next().unwrap().parse::<usize>().unwrap();
+            let end = range.next().unwrap().parse::<usize>().unwrap();
+            let char = split.next().unwrap().chars().next().unwrap();
+            let pw = split.next().unwrap();
+            (start, end, char, pw)
+        })
+        .collect::<Vec<_>>();
+
+    println!("Passwords: {:?}", passwords);
     println!("Part One: {}", "TODO");
     println!("Part Two: {}", "TODO");
 }
