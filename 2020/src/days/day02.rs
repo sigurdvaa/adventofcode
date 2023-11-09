8b3df5797769e59465e7a2a5cbc4395c1cf48b87
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2023-11-09 23:30:55 +0100

    2020/02

--- 2020/src/days/day02.rs
+++ 2020/src/days/day02.rs
@@ -1,37 +1,80 @@
 use std::fs;
 
+struct Password<'a> {
+    min: usize,
+    max: usize,
+    letter: char,
+    password: &'a str,
+}
+
+fn count_valid_passwords_char_count(passwords: &Vec<Password>) -> usize {
+    passwords
+        .iter()
+        .filter(|pw| {
+            let count = pw.password.chars().filter(|&c| c == pw.letter).count();
+            pw.min <= count && count <= pw.max
+        })
+        .count()
+}
+
+fn count_valid_passwords_char_pos(passwords: &Vec<Password>) -> usize {
+    passwords
+        .iter()
+        .filter(|pw| {
+            let chars = pw.password.chars().collect::<Vec<_>>();
+            let a = chars[pw.min - 1];
+            let b = chars[pw.max - 1];
+            (a == pw.letter || b == pw.letter) && a != b
+        })
+        .count()
+}
+
+fn parse_policy_and_password(input: &str) -> Vec<Password> {
+    input
+        .lines()
+        .filter(|&line| line != "")
+        .map(|line| {
+            let mut split = line.split(" ");
+            let mut range = split.next().unwrap().split("-");
+            let min = range.next().unwrap().parse::<usize>().unwrap();
+            let max = range.next().unwrap().parse::<usize>().unwrap();
+            let letter = split.next().unwrap().chars().next().unwrap();
+            let password = split.next().unwrap();
+            Password {
+                min,
+                max,
+                letter,
+                password,
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
 pub fn run() {
     println!("Day 2: Password Philosophy");
     let file_path = "inputs/day02.txt";
     let input_raw =
         fs::read_to_string(file_path).expect(format!("Error reading file '{file_path}'").as_str());
 
-    let passwords = input_raw
-        .lines()
-        .filter(|&line| line != "")
-        .map(|line| {
-            let mut split = line.split(" ");
-            let mut range = split.next().unwrap().split("-");
-            let start = range.next().unwrap().parse::<usize>().unwrap();
-            let end = range.next().unwrap().parse::<usize>().unwrap();
-            let char = split.next().unwrap().chars().next().unwrap();
-            let pw = split.next().unwrap();
-            (start, end, char, pw)
-        })
-        .collect::<Vec<_>>();
-
-    println!("Passwords: {:?}", passwords);
-    println!("Part One: {}", "TODO");
-    println!("Part Two: {}", "TODO");
+    let passwords = parse_policy_and_password(&input_raw);
+    println!("Part One: {}", count_valid_passwords_char_count(&passwords));
+    println!("Part Two: {}", count_valid_passwords_char_pos(&passwords));
 }
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
+    const INPUT_TEST: &'static str = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc";
 
     #[test]
-    fn test_part_one() {}
+    fn test_part_one() {
+        let passwords = parse_policy_and_password(&INPUT_TEST);
+        assert_eq!(count_valid_passwords_char_count(&passwords), 2);
+    }
 
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        let passwords = parse_policy_and_password(&INPUT_TEST);
+        assert_eq!(count_valid_passwords_char_pos(&passwords), 1);
+    }
 }
