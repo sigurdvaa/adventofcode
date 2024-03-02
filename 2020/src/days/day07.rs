897df3b45cdb3f11d254781fdea78b4390a192e1
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2024-03-02 17:33:42 +0100

    2020/07

--- 2020/src/days/day07.rs
+++ 2020/src/days/day07.rs
@@ -1,5 +1,11 @@
 use std::fs;
 
+fn parse_rules(rules: &str) -> _ {}
+
+fn num_bags_can_contain_bag(rules: &str, bag: &str) -> usize {
+    0
+}
+
 pub fn run() {
     println!("Day 7: Handy Haversacks");
     let file_path = "inputs/day07.txt";
@@ -15,7 +21,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {}
+    fn test_part_one() {
+        const INPUT_TEST: &str = concat!(
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.\n",
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n",
+            "bright white bags contain 1 shiny gold bag.\n",
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n",
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n",
+            "faded blue bags contain no other bags.\n",
+            "dotted black bags contain no other bags.",
+        );
+        let mybag = "shiny gold";
+        assert_eq!(num_bags_can_contain_bag(INPUT_TEST, mybag), 4);
+    }
 
     #[test]
     fn test_part_two() {}
