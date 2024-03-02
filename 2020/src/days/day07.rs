e4c12e6ec0ee7056349e9dad43f8bfb1641b8870
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2024-03-02 18:45:46 +0100

    2020/07

--- 2020/src/days/day07.rs
+++ 2020/src/days/day07.rs
@@ -1,8 +1,26 @@
 use std::fs;
 
-fn parse_rules(rules: &str) -> _ {}
+const MYBAG: &str = "shiny gold";
 
 fn num_bags_can_contain_bag(rules: &str, bag: &str) -> usize {
+    let mut outer = vec![bag];
+    let lines = rules.lines().collect::<Vec<_>>();
+    let mut i = 0;
+    while i < outer.len() {
+        for line in lines.iter() {
+            let mut split = line.split(" bags contain ");
+            let name = split.next().unwrap();
+            let contain = split.next().unwrap();
+            if contain.contains(outer[i]) && !outer.contains(&name) {
+                outer.push(name);
+            }
+        }
+        i += 1;
+    }
+    outer.len() - 1
+}
+
+fn num_bags_in_bag(rules: &str, bag: &str) -> usize {
     0
 }
 
@@ -12,7 +30,7 @@ pub fn run() {
     let input_raw = fs::read_to_string(file_path)
         .unwrap_or_else(|err| panic!("Error reading file '{file_path}': {err}"));
 
-    println!("Part One: {}", "TODO");
+    println!("Part One: {}", num_bags_can_contain_bag(&input_raw, MYBAG));
     println!("Part Two: {}", "TODO");
 }
 
@@ -33,10 +51,20 @@ mod tests {
             "faded blue bags contain no other bags.\n",
             "dotted black bags contain no other bags.",
         );
-        let mybag = "shiny gold";
-        assert_eq!(num_bags_can_contain_bag(INPUT_TEST, mybag), 4);
+        assert_eq!(num_bags_can_contain_bag(INPUT_TEST, MYBAG), 4);
     }
 
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        const INPUT_TEST: &str = concat!(
+            "shiny gold bags contain 2 dark red bags.\n",
+            "dark red bags contain 2 dark orange bags.\n",
+            "dark orange bags contain 2 dark yellow bags.\n",
+            "dark yellow bags contain 2 dark green bags.\n",
+            "dark green bags contain 2 dark blue bags.\n",
+            "dark blue bags contain 2 dark violet bags.\n",
+            "dark violet bags contain no other bags.",
+        );
+        assert_eq!(num_bags_in_bag(INPUT_TEST, MYBAG), 126);
+    }
 }
