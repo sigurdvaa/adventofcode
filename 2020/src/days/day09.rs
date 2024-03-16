f05600aa94fc2008bf49e1d8c189a9aed7b43341
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2024-03-16 16:02:38 +0100

    2020/09

--- 2020/src/days/day09.rs
+++ 2020/src/days/day09.rs
@@ -28,6 +28,31 @@ fn first_invalid_number(numbers: &[usize], preamble: usize) -> Option<usize> {
     None
 }
 
+fn find_encryption_weakness(numbers: &[usize], target: usize) -> Option<usize> {
+    let mut lo = 0;
+    let mut hi = 1;
+    let mut sum = numbers[lo];
+    let len = numbers.len();
+    while lo < len {
+        if sum < target && hi < len {
+            sum += numbers[hi];
+            hi += 1;
+        }
+
+        if sum > target {
+            sum -= numbers[lo];
+            lo += 1;
+        }
+
+        if sum == target {
+            let min = numbers[lo..hi].iter().min().unwrap();
+            let max = numbers[lo..hi].iter().max().unwrap();
+            return Some(min + max);
+        }
+    }
+    None
+}
+
 pub fn run() {
     println!("Day 9: Encoding Error");
     let file_path = "inputs/day09.txt";
@@ -35,24 +60,31 @@ pub fn run() {
         .unwrap_or_else(|err| panic!("Error reading file '{file_path}': {err}"));
 
     let numbers = parse_numbers(&input_raw);
-    println!("Part One: {}", first_invalid_number(&numbers, 25).unwrap());
-    println!("Part Two: {}", "TODO");
+    let invalid = first_invalid_number(&numbers, 25).unwrap();
+    println!("Part One: {}", invalid);
+    println!(
+        "Part Two: {}",
+        find_encryption_weakness(&numbers, invalid).unwrap()
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const INPUT_TEST: &str = concat!(
+        "35\n", "20\n", "15\n", "25\n", "47\n", "40\n", "62\n", "55\n", "65\n", "95\n", "102\n",
+        "117\n", "150\n", "182\n", "127\n", "219\n", "299\n", "277\n", "309\n", "576",
+    );
 
     #[test]
     fn test_part_one() {
-        const INPUT_TEST: &str = concat!(
-            "35\n", "20\n", "15\n", "25\n", "47\n", "40\n", "62\n", "55\n", "65\n", "95\n",
-            "102\n", "117\n", "150\n", "182\n", "127\n", "219\n", "299\n", "277\n", "309\n", "576",
-        );
         let numbers = parse_numbers(INPUT_TEST);
         assert_eq!(first_invalid_number(&numbers, 5), Some(127));
     }
 
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        let numbers = parse_numbers(INPUT_TEST);
+        assert_eq!(find_encryption_weakness(&numbers, 127), Some(62));
+    }
 }
