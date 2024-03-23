82104927b1cb7dba29c7111ac7cd0d9c6d685520
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2024-03-24 00:00:56 +0100

    2020/11

--- 2020/src/days/day11.rs
+++ 2020/src/days/day11.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-fn find_neighbors_immediately(
+fn find_seat_neighbors_immediately(
     cells: &[char],
     width: usize,
     seat: char,
     floor: char,
-) -> Vec<Vec<usize>> {
-    let mut neighbors = vec![];
+) -> Vec<(usize, Vec<usize>)> {
+    let mut seat_neighbors = vec![];
     let len = (cells.len() / width) as i32;
     let width = width as i32;
     let dirs: &[(i32, i32)] = &[
@@ -21,11 +21,11 @@ fn find_neighbors_immediately(
     ];
 
     for i in 0..cells.len() {
-        neighbors.push(vec![]);
         if cells[i] == floor {
             continue;
         }
 
+        let mut neighbors = vec![];
         let x = i as i32 % width;
         let y = i as i32 / width;
 
@@ -38,27 +38,26 @@ fn find_neighbors_immediately(
                 && dy < len
                 && cells[((dy * width) + dx) as usize] == seat
             {
-                neighbors[i].push(((dy * width) + dx) as usize);
+                neighbors.push(((dy * width) + dx) as usize);
             }
         }
+        seat_neighbors.push((i, neighbors));
     }
-    neighbors
+    seat_neighbors
 }
 
 fn occupied_cells_immediately(cells: &[char], width: usize) -> usize {
     const OCCUPIED: char = '#';
     const EMPTY: char = 'L';
     const FLOOR: char = '.';
-    let neighbors = find_neighbors_immediately(cells, width, EMPTY, FLOOR);
+    let seat_neighbors = find_seat_neighbors_immediately(cells, width, EMPTY, FLOOR);
     let mut occupied_cells = cells.iter().map(|&c| c == OCCUPIED).collect::<Vec<bool>>();
     let mut change = vec![];
     loop {
-        for (i, cell) in occupied_cells.iter().enumerate() {
-            if neighbors[i].is_empty() {
-                continue;
-            }
-            let count = neighbors[i].iter().filter(|&n| occupied_cells[*n]).count();
-            if (!*cell && count == 0) || (*cell && count > 3) {
+        for (i, neighbors) in &seat_neighbors {
+            let count = neighbors.iter().filter(|&n| occupied_cells[*n]).count();
+            let cell = occupied_cells[*i];
+            if (!cell && count == 0) || (cell && count > 3) {
                 change.push(i);
             }
         }
@@ -66,21 +65,20 @@ fn occupied_cells_immediately(cells: &[char], width: usize) -> usize {
         if change.is_empty() {
             break;
         }
-        for &i in &change {
-            occupied_cells[i] = !occupied_cells[i];
+        for i in change.drain(..) {
+            occupied_cells[*i] = !occupied_cells[*i];
         }
-        change.clear();
     }
     occupied_cells.iter().filter(|&c| *c).count()
 }
 
-fn find_neighbors_direction(
+fn find_seat_neighbors_direction(
     cells: &[char],
     width: usize,
     seat: char,
     floor: char,
-) -> Vec<Vec<usize>> {
-    let mut neighbors = vec![];
+) -> Vec<(usize, Vec<usize>)> {
+    let mut seat_neighbors = vec![];
     let len = (cells.len() / width) as i32;
     let width = width as i32;
     let dirs: &[(i32, i32)] = &[
@@ -95,11 +93,11 @@ fn find_neighbors_direction(
     ];
 
     for i in 0..cells.len() {
-        neighbors.push(vec![]);
         if cells[i] == floor {
             continue;
         }
 
+        let mut neighbors = vec![];
         let x = i as i32 % width;
         let y = i as i32 / width;
 
@@ -109,31 +107,30 @@ fn find_neighbors_direction(
             while dx >= 0 && dy >= 0 && dx < width && dy < len {
                 let di = ((dy * width) + dx) as usize;
                 if cells[di] == seat {
-                    neighbors[i].push(di);
+                    neighbors.push(di);
                     break;
                 }
                 dx += dir.0;
                 dy += dir.1;
             }
         }
+        seat_neighbors.push((i, neighbors));
     }
-    neighbors
+    seat_neighbors
 }
 
 fn occupied_cells_direction(cells: &[char], width: usize) -> usize {
     const OCCUPIED: char = '#';
     const EMPTY: char = 'L';
     const FLOOR: char = '.';
-    let neighbors = find_neighbors_direction(cells, width, EMPTY, FLOOR);
+    let seat_neighbors = find_seat_neighbors_direction(cells, width, EMPTY, FLOOR);
     let mut occupied_cells = cells.iter().map(|&c| c == OCCUPIED).collect::<Vec<bool>>();
     let mut change = vec![];
     loop {
-        for (i, cell) in occupied_cells.iter().enumerate() {
-            if neighbors[i].is_empty() {
-                continue;
-            }
-            let count = neighbors[i].iter().filter(|&n| occupied_cells[*n]).count();
-            if (!*cell && count == 0) || (*cell && count > 4) {
+        for (i, neighbors) in &seat_neighbors {
+            let count = neighbors.iter().filter(|&n| occupied_cells[*n]).count();
+            let cell = occupied_cells[*i];
+            if (!cell && count == 0) || (cell && count > 4) {
                 change.push(i);
             }
         }
@@ -141,10 +138,9 @@ fn occupied_cells_direction(cells: &[char], width: usize) -> usize {
         if change.is_empty() {
             break;
         }
-        for &i in &change {
-            occupied_cells[i] = !occupied_cells[i];
+        for i in change.drain(..) {
+            occupied_cells[*i] = !occupied_cells[*i];
         }
-        change.clear();
     }
     occupied_cells.iter().filter(|&c| *c).count()
 }
