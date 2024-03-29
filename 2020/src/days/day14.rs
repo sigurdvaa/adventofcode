49b0a19c89c5bb9383f7569a0036f7df4b1aeb40
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2024-03-29 20:44:52 +0100

    2020/14

--- 2020/src/days/day14.rs
+++ 2020/src/days/day14.rs
@@ -1,84 +1,130 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
-#[derive(Clone, Debug, PartialEq)]
-enum Bit {
-    Zero,
-    One,
-    Nop,
-}
-
-struct Init {
-    mask: Vec<Bit>,
+struct Ins {
     addr: usize,
+    mask_ones: u64,
+    mask_zeros: u64,
     value: u64,
 }
 
-fn parse_prog(input: &str) -> Vec<Init> {
-    let mut inits = Vec::new();
-    let mut mask: Option<Vec<Bit>> = None;
+fn parse_prog_v1(input: &str) -> Vec<Ins> {
+    let mut prog = Vec::new();
+    let mut mask_zeros = 0;
+    let mut mask_ones = 0;
     for line in input.lines() {
         match line {
             line if line.starts_with("mask = ") => {
-                mask = Some(
-                    line[7..]
-                        .chars()
-                        .map(|c| match c {
-                            '1' => Bit::One,
-                            '0' => Bit::Zero,
-                            _ => Bit::Nop,
-                        })
-                        .collect(),
-                );
+                mask_zeros = 0;
+                mask_ones = 0;
+                for c in line[7..].chars() {
+                    mask_zeros <<= 1;
+                    mask_ones <<= 1;
+                    match c {
+                        '0' => mask_zeros |= 1,
+                        '1' => mask_ones |= 1,
+                        _ => (),
+                    }
+                }
             }
-            line if line.starts_with("mem[") && mask.is_some() => {
-                let chars = line.chars().collect::<Vec<_>>();
-                if let Some(addr_end) = chars.iter().position(|c| *c == ']') {
-                    let addr = chars[4..addr_end]
-                        .iter()
-                        .collect::<String>()
-                        .parse::<usize>()
-                        .unwrap();
-                    if let Some(value_start) = chars.iter().position(|c| *c == '=') {
-                        let value = chars[value_start + 2..]
-                            .iter()
-                            .collect::<String>()
-                            .parse::<u64>()
-                            .unwrap();
-                        inits.push(Init {
-                            mask: mask.clone().unwrap(),
+            line if line.starts_with("mem[") => {
+                if let Some(addr_end) = line.find(']') {
+                    let addr = line[4..addr_end].parse::<usize>().unwrap();
+                    if let Some(value_start) = line.find('=') {
+                        let value = line[value_start + 2..].parse::<u64>().unwrap();
+                        prog.push(Ins {
                             addr,
+                            mask_ones,
+                            mask_zeros,
                             value,
                         });
                     }
                 }
             }
-            _ => panic!("Invalid initializer: {}", line),
+            _ => panic!("invalid initializer: {}", line),
         }
     }
-    inits
+    prog
 }
 
-fn sum_mem_after_init(inits: &[Init]) -> u64 {
+fn sum_after_init_v1(prog: &[Ins]) -> u64 {
     let mut mem = vec![];
-
-    for init in inits {
-        let mut value = init.value;
-        let mask_len = init.mask.len() - 1;
-        for (i, bit) in init.mask.iter().enumerate() {
-            match bit {
-                Bit::One => value |= 1 << (mask_len - i),
-                Bit::Zero => value &= !(1 << (mask_len - i)),
-                Bit::Nop => (),
-            }
+    for ins in prog {
+        if ins.addr >= mem.len() {
+            mem.resize(ins.addr + 1, 0);
         }
-
-        if init.addr >= mem.len() {
-            mem.resize(init.addr + 1, 0);
-        }
-        mem[init.addr] = value;
+        mem[ins.addr] = ins.value;
+        mem[ins.addr] |= ins.mask_ones;
+        mem[ins.addr] &= !(ins.mask_zeros);
     }
-
     mem.iter().sum()
+}
+
+struct Ins2 {
+    addr: usize,
+    mask: u64,
+    floats: Vec<usize>,
+    value: u64,
+}
+
+fn parse_prog_v2(input: &str) -> Vec<Ins2> {
+    let mut prog = Vec::new();
+    let mut mask = 0;
+    let mut floats = Vec::new();
+    for line in input.lines() {
+        match line {
+            line if line.starts_with("mask = ") => {
+                floats.clear();
+                mask = 0;
+                let mask_len = line[7..].len() - 1;
+                for (i, c) in line[7..].chars().enumerate() {
+                    mask <<= 1;
+                    match c {
+                        '1' => mask |= 1,
+                        'X' => floats.push(mask_len - i),
+                        _ => (),
+                    }
+                }
+            }
+            line if line.starts_with("mem[") => {
+                if let Some(addr_end) = line.find(']') {
+                    let addr = line[4..addr_end].parse::<usize>().unwrap();
+                    if let Some(value_start) = line.find('=') {
+                        let value = line[value_start + 2..].parse::<u64>().unwrap();
+                        prog.push(Ins2 {
+                            addr,
+                            mask,
+                            floats: floats.clone(),
+                            value,
+                        });
+                    }
+                }
+            }
+            _ => panic!("invalid initializer: {}", line),
+        }
+    }
+    prog
+}
+
+fn sum_after_init_v2(prog: &[Ins2]) -> u64 {
+    let mut mem = HashMap::new();
+    for ins in prog {
+        let mut addrs_curr = vec![ins.addr | ins.mask as usize];
+        let mut addrs_next = vec![];
+
+        for i in ins.floats.iter() {
+            addrs_next.clear();
+            for addr in addrs_curr.iter() {
+                addrs_next.push(*addr | (1 << i));
+                addrs_next.push(*addr & !(1 << i));
+            }
+            std::mem::swap(&mut addrs_curr, &mut addrs_next);
+        }
+
+        for addr in addrs_curr.iter() {
+            mem.insert(*addr, ins.value);
+        }
+    }
+    mem.values().sum()
 }
 
 pub fn run() {
@@ -87,34 +133,42 @@ pub fn run() {
     let input_raw = fs::read_to_string(file_path)
         .unwrap_or_else(|err| panic!("Error reading file '{file_path}': {err}"));
 
-    let init_prog = parse_prog(&input_raw);
-    println!("Part One: {}", sum_mem_after_init(&init_prog));
-    println!("Part Two: {}", "TODO");
+    let prog = parse_prog_v1(&input_raw);
+    println!("Part One: {}", sum_after_init_v1(&prog));
+    let prog = parse_prog_v2(&input_raw);
+    println!("Part Two: {}", sum_after_init_v2(&prog));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT_TEST: &str = concat!(
-        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\n",
-        "mem[8] = 11\n",
-        "mem[7] = 101\n",
-        "mem[8] = 0",
-    );
-
     #[test]
     fn test_part_one() {
-        let init_prog = parse_prog(INPUT_TEST);
-        assert_eq!(
-            init_prog[0].mask.iter().rev().take(2).collect::<Vec<_>>(),
-            &[&Bit::Nop, &Bit::Zero]
+        const INPUT_TEST: &str = concat!(
+            "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\n",
+            "mem[8] = 11\n",
+            "mem[7] = 101\n",
+            "mem[8] = 0",
         );
-        assert_eq!(init_prog[0].addr, 8);
-        assert_eq!(init_prog[0].value, 11);
-        assert_eq!(sum_mem_after_init(&init_prog), 165);
+
+        let prog = parse_prog_v1(INPUT_TEST);
+        assert_eq!(prog[0].addr, 8);
+        assert_eq!(prog[0].mask_ones, 64);
+        assert_eq!(prog[0].mask_zeros, 2);
+        assert_eq!(prog[0].value, 11);
+        assert_eq!(sum_after_init_v1(&prog), 165);
     }
 
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        const INPUT_TEST: &str = concat!(
+            "mask = 000000000000000000000000000000X1001X\n",
+            "mem[42] = 100\n",
+            "mask = 00000000000000000000000000000000X0XX\n",
+            "mem[26] = 1",
+        );
+        let prog = parse_prog_v2(INPUT_TEST);
+        assert_eq!(sum_after_init_v2(&prog), 208);
+    }
 }
