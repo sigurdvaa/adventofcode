5209b005914381d0f02d686c4438dea86cccb883
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2024-05-09 22:28:44 +0200

    2020/20

--- 2020/src/days/day20.rs
+++ 2020/src/days/day20.rs
@@ -58,33 +58,98 @@ fn tiles_match(a: &Tile, b: &Tile) -> bool {
     false
 }
 
-fn find_matching_tiles(tiles: &[Tile]) -> usize {
-    let mut matches = vec![0; tiles.len()];
+fn find_matching_tiles(tiles: &[Tile]) -> Vec<Vec<usize>> {
+    let mut graph = vec![vec![]; tiles.len()];
     for i in 0..tiles.len() {
         for s in 0..tiles.len() {
             if tiles[i].id == tiles[s].id {
                 continue;
             }
             if tiles_match(&tiles[i], &tiles[s]) {
-                matches[i] += 1;
+                graph[i].push(s);
             }
         }
     }
+    graph
+}
 
+fn corners_product(tiles: &[Tile], graph: &[Vec<usize>]) -> usize {
     let mut prod = 1;
-    for (i, n) in matches.iter().enumerate() {
-        if *n == 2 {
+    for (i, n) in graph.iter().enumerate() {
+        if n.len() == 2 {
             prod *= tiles[i].id as usize;
         }
     }
     prod
 }
 
+fn build_image<'a>(tiles: &'a [Tile], graph: &[Vec<usize>]) -> Vec<Vec<&'a Tile>> {
+    let mut used = vec![false; tiles.len()];
+    let mut image = vec![];
+
+    // first row
+    let first = graph.iter().position(|edges| edges.len() == 2).unwrap();
+    used[first] = true;
+    let mut row = vec![first];
+    loop {
+        let tile = &tiles[*row.last().unwrap()];
+
+        let mut found_edge = false;
+        for (i, edges) in graph.iter().enumerate() {
+            if !used[i] && edges.len() == 3 && tiles_match(tile, &tiles[i]) {
+                used[i] = true;
+                row.push(i);
+                found_edge = true;
+                break;
+            }
+        }
+        if found_edge {
+            continue;
+        }
+
+        for (i, edges) in graph.iter().enumerate() {
+            if !used[i] && edges.len() == 2 && tiles_match(tile, &tiles[i]) {
+                used[i] = true;
+                row.push(i);
+                break;
+            }
+        }
+        break;
+    }
+    image.push(row);
+
+    // rest of the rows
+    loop {
+        let prev = image.last().unwrap();
+        let mut row = vec![];
+        for i in prev {
+            for n in graph[*i].iter() {
+                if !used[*n] && tiles_match(&tiles[*i], &tiles[*n]) {
+                    used[*n] = true;
+                    row.push(*n);
+                    break;
+                }
+            }
+        }
+        if row.is_empty() {
+            break;
+        }
+        image.push(row);
+    }
+
+    image
+        .iter()
+        .map(|row| row.iter().map(|i| &tiles[*i]).collect())
+        .collect()
+}
+
 pub fn run() {
     let input_raw = crate::load_input(module_path!());
     let tiles = parse_tiles(&input_raw);
+    let graph = find_matching_tiles(&tiles);
+    let image = build_image(&tiles, &graph);
     println!("Day 20: Jurassic Jigsaw");
-    println!("Part One: {}", find_matching_tiles(&tiles));
+    println!("Part One: {}", corners_product(&tiles, &graph));
     println!("Part Two: {}", "TODO");
 }
 
@@ -205,9 +270,24 @@ mod tests {
     #[test]
     fn test_part_one() {
         let tiles = parse_tiles(INPUT_TEST);
-        assert_eq!(find_matching_tiles(&tiles), 20899048083289);
+        let graph = find_matching_tiles(&tiles);
+        assert_eq!(corners_product(&tiles, &graph), 20899048083289);
     }
 
     #[test]
-    fn test_part_two() {}
+    fn test_part_two() {
+        let tiles = parse_tiles(INPUT_TEST);
+        let graph = find_matching_tiles(&tiles);
+        let image = build_image(&tiles, &graph);
+        for row in image {
+            println!(
+                "{}",
+                row.iter()
+                    .map(|t| t.id.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+        }
+        assert_eq!(false, true);
+    }
 }
