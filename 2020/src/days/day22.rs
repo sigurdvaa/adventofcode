0de6f789ef635315d8d3e0d3272b70aa31a287a9
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2024-10-26 14:11:06 +0200

    2020/22

---
+++ 2020/src/days/day22.rs
@@ -0,0 +1,34 @@
+pub fn run() {
+    let input_raw = crate::load_input(module_path!());
+    println!("Day 00: Crab Combat");
+    println!("Part One: {}", "TODO");
+    println!("Part Two: {}", "TODO");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_TEST: &str = concat!(
+        "Player 1:\n",
+        "9\n",
+        "2\n",
+        "6\n",
+        "3\n",
+        "1\n",
+        "\n",
+        "Player 2:\n",
+        "5\n",
+        "8\n",
+        "4\n",
+        "7\n",
+        "10\n",
+        ""
+    );
+
+    #[test]
+    fn test_part_one() {}
+
+    #[test]
+    fn test_part_two() {}
+}
