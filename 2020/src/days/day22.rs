8adb9b430ae45337ad699060acc16d678bdf62ec
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2024-10-30 20:35:42 +0100

    2020/22

--- 2020/src/days/day22.rs
+++ 2020/src/days/day22.rs
@@ -1,7 +1,53 @@
+use std::collections::VecDeque;
+
+fn parse_decks(input: &str) -> (VecDeque<usize>, VecDeque<usize>) {
+    let mut lines = input.lines();
+    let mut p1 = VecDeque::new();
+    let mut p2 = VecDeque::new();
+
+    if let Some("Player 1:") = lines.next() {
+        for card in lines.by_ref() {
+            match card {
+                "" => break,
+                n => p1.push_back(n.parse().expect("")),
+            }
+        }
+    }
+
+    if let Some("Player 2:") = lines.next() {
+        for card in lines {
+            match card {
+                "" => break,
+                n => p2.push_back(n.parse().expect("")),
+            }
+        }
+    }
+
+    (p1, p2)
+}
+
+fn run_game(mut p1: VecDeque<usize>, mut p2: VecDeque<usize>) -> usize {
+    while !p1.is_empty() && !p2.is_empty() {
+        let v1 = p1.pop_front().expect("deck can't be empty while playing");
+        let v2 = p2.pop_front().expect("deck can't be empty while playing");
+        if v1 > v2 {
+            p1.push_back(v1);
+            p1.push_back(v2);
+        } else {
+            p2.push_back(v2);
+            p2.push_back(v1);
+        }
+    }
+
+    let winner = if p1.is_empty() { p2 } else { p1 };
+    winner.iter().enumerate().map(|(i, val)| (winner.len() - i) * val).sum()
+}
+
 pub fn run() {
     let input_raw = crate::load_input(module_path!());
-    println!("Day 00: Crab Combat");
-    println!("Part One: {}", "TODO");
+    let (p1, p2) = parse_decks(&input_raw);
+    println!("Day 22: Crab Combat");
+    println!("Part One: {}", run_game(p1, p2));
     println!("Part Two: {}", "TODO");
 }
 
@@ -27,7 +73,10 @@ mod tests {
     );
 
     #[test]
-    fn test_part_one() {}
+    fn test_part_one() {
+        let (p1, p2) = parse_decks(INPUT_TEST);
+        assert_eq!(run_game(p1, p2), 306);
+    }
 
     #[test]
     fn test_part_two() {}
