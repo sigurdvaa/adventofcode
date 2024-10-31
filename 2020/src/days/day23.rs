1593bf7666a3af41191b35e4b80fb9b6de95ca06
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2024-10-31 21:20:05 +0100

    2020/23

---
+++ 2020/src/days/day23.rs
@@ -0,0 +1,19 @@
+pub fn run() {
+    let input_raw = "123487596";
+    println!("Day 23: Crab Cups");
+    println!("Part One: {}", "TODO");
+    println!("Part Two: {}", "TODO");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_TEST: &str = "389125467";
+
+    #[test]
+    fn test_part_one() {}
+
+    #[test]
+    fn test_part_two() {}
+}
