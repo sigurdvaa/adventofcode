a0cb18cebff7359e218c7a731e15315015a395c4
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2024-11-23 01:19:02 +0100

    2020/25

---
+++ 2020/src/days/day25.rs
@@ -0,0 +1,17 @@
+pub fn run() {
+    let input_raw = crate::load_input(module_path!());
+    println!("Day 25: Combo Breaker");
+    println!("Part One: {}", "TODO");
+    println!("Part Two: {}", "TODO");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {}
+
+    #[test]
+    fn test_part_two() {}
+}
