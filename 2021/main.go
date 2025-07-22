ceb8576076a8501441a975d5a14b642e424aee44
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-07-22 14:14:49 +0200

    2021 init

--- 2021/main.go
+++ 2021/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+	lastArg := os.Args[len(os.Args)-1]
+
+	fmt.Println("argsWithProg: ", argsWithProg)
+	fmt.Println("argsWithoutProg: ", argsWithoutProg)
+	fmt.Println("lastArg: ", lastArg)
+
+	var num int
+	flag.IntVar(&num, "d", 0, "Specify which day of the Advent of Code to run. Default is 0 (all)")
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [-d <0-25>]", os.Args[0])
+	}
+	flag.Parse()
+	fmt.Println("Day: ", num)
+	if num > 25 {
+		fmt.Printf("Day is out of range (0-25): %d\n", num)
+		os.Exit(1)
+	}
 }
