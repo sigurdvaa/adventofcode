337d1b57d011c86fa601d4b88741cd00758ecaf3
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-07-23 21:27:37 +0200

    2021/01

--- 2021/days/day01/main.go
+++ 2021/days/day01/main.go
@@ -3,16 +3,47 @@ package day01
 import (
 	"aoc_2021/input"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
 
 func Run() {
+	fmt.Println("Day 1: Sonar Sweep")
 	part1()
 	part2()
 }
 
 func part1() {
-	rawInput := input.ReadDay(1)
-	fmt.Println(rawInput)
+	inputString := input.ReadDay(1)
+
+	var depths []int
+	count := 0
+	prev := 0
+
+	for _, line := range strings.Split(inputString, "\n") {
+		if line == "" {
+			continue
+		}
+
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		depths = append(depths, num)
+
+		if prev != 0 {
+			if num > prev {
+				count += 1
+			}
+		}
+		prev = num
+
+	}
+
+	fmt.Printf("Part One: %d\n", count)
 }
 
-func part2() {}
+func part2() {
+	fmt.Println("Part Two: TODO")
+}
--- 2021/main.go
+++ 2021/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	var num int
-	flag.IntVar(&num, "d", 0, "Specify which day of the Advent of Code to run. Default is 0 (all)")
+	flag.IntVar(&num, "d", 0, "Specify which day of the Advent of Code to run. Default is 0 (all).")
 	flag.Usage = func() {
 		fmt.Printf("Usage: %s [-d <0-25>]", os.Args[0])
 	}
