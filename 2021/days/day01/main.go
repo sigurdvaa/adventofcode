998339a2454a969082f9420085852362a81e8dbf
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-07-24 19:09:02 +0200

    2021/01

--- 2021/days/day01/main.go
+++ 2021/days/day01/main.go
@@ -10,17 +10,9 @@ import (
 
 func Run() {
 	fmt.Println("Day 1: Sonar Sweep")
-	part1()
-	part2()
-}
 
-func part1() {
 	inputString := input.ReadDay(1)
-
 	var depths []int
-	count := 0
-	prev := 0
-
 	for _, line := range strings.Split(inputString, "\n") {
 		if line == "" {
 			continue
@@ -31,19 +23,45 @@ func part1() {
 			log.Fatal(err)
 		}
 		depths = append(depths, num)
+	}
 
-		if prev != 0 {
-			if num > prev {
-				count += 1
-			}
+	part1(depths)
+	part2(depths)
+}
+
+func part1(depths []int) {
+	count := 0
+
+	for i := 1; i < len(depths); i++ {
+		if depths[i] > depths[i-1] {
+			count += 1
 		}
-		prev = num
-
 	}
 
 	fmt.Printf("Part One: %d\n", count)
 }
 
-func part2() {
-	fmt.Println("Part Two: TODO")
+func sum(arr []int) int {
+	_sum := 0
+	for _, val := range arr {
+		_sum += val
+	}
+	return _sum
+}
+
+func part2(depths []int) {
+	count := 0
+	prev := 0
+
+	for i := range depths {
+		num := sum(depths[i : i+3])
+
+		if prev != 0 && num > prev {
+			count += 1
+		}
+
+		prev = num
+	}
+
+	fmt.Printf("Part Two: %d\n", count)
 }
