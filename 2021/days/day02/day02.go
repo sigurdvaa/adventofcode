c63c6289d804f274993a08641a4db85f25d97f2a
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-07-24 22:40:47 +0200

    2021/02

--- 2021/days/day02/day02.go
+++ 2021/days/day02/day02.go
@@ -1,15 +1,63 @@
 package day02
 
 import (
-	// "aoc_2021/input"
+	"aoc_2021/input"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
+
+type pos struct {
+	x int
+	y int
+}
+
+func parseInput(inputString string) []pos {
+	var ins []pos
+	for _, line := range strings.Split(inputString, "\n") {
+		if line == "" {
+			continue
+		}
+
+		split := strings.Split(line, " ")
+		num, err := strconv.Atoi(split[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		x, y := 0, 0
+		switch split[0] {
+		case "forward":
+			x = num
+		case "down":
+			y = num
+		case "up":
+			y = num * -1
+		default:
+			log.Fatalf("unknown direction: %s", split[0])
+		}
+
+		ins = append(ins, pos{x, y})
+	}
+	return ins
+}
+
+func positionProduct(ins []pos) int {
+	x, y := 0, 0
+	for _, pos := range ins {
+		x += pos.x
+		y += pos.y
+	}
+	return x * y
+}
 
 func Run() {
 	fmt.Println("Day 2: Dive!")
 
-	// inputString := input.ReadDay(2)
+	inputString := input.ReadDay(2)
+	ins := parseInput(inputString)
 
-	fmt.Printf("Part One: TODO\n")
+	fmt.Printf("Part One: %d\n", positionProduct(ins))
 	fmt.Printf("Part Two: TODO\n")
 }
--- 2021/days/day02/day02_test.go
+++ 2021/days/day02/day02_test.go
@@ -4,9 +4,19 @@ import (
 	"testing"
 )
 
+var inputString string = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+`
+
+var ins []pos = parseInput(inputString)
+
 func TestPartOne(t *testing.T) {
-	got := 1
-	want := 2
+	got := positionProduct(ins)
+	want := 150
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
@@ -14,4 +24,10 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
+	got := 1
+	want := 900
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
