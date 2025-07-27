8ebd6af069c53204195bcfe78ab42e62a0282d23
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-07-27 20:47:53 +0200

    2021/05

--- 2021/days/day05/day05.go
+++ 2021/days/day05/day05.go
@@ -1,14 +1,59 @@
 package day05
 
 import (
-	//"aoc_2021/input"
+	"aoc_2021/input"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
 
-func Run() {
-	fmt.Println("Day XX: Example")
+type Pos struct {
+	x int
+	y int
+}
 
-	//inputString := input.ReadDay("day05")
+type Vent struct {
+	start Pos
+	end   Pos
+}
+
+func parsePos(str string) Pos {
+	split := strings.Split(str, ",")
+
+	x, err := strconv.Atoi(split[0])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	y, err := strconv.Atoi(split[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return Pos{x, y}
+}
+
+func parseInput(stringInput string) []Vent {
+	var vents []Vent
+	for _, line := range strings.Split(stringInput, "\n") {
+		if line == "" {
+			continue
+		}
+		split := strings.Split(line, " -> ")
+		start := parsePos(split[0])
+		end := parsePos(split[1])
+		vents = append(vents, Vent{start, end})
+	}
+	return vents
+}
+
+func Run() {
+	fmt.Println("Day 5: Hydrothermal Venture")
+
+	inputString := input.ReadDay("day05")
+	vents := parseInput(inputString)
+	_ = vents
 
 	fmt.Printf("Part One: TODO\n")
 	fmt.Printf("Part Two: TODO\n")
--- 2021/days/day05/day05_test.go
+++ 2021/days/day05/day05_test.go
@@ -1,18 +1,31 @@
 package day05
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestPartOne(t *testing.T) {
-	/*
-	   got := 1
-	   want := 2
+var inputString string = `
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+`
 
-	   	if got != want {
-	   		t.Errorf("got %d, wanted %d", got, want)
-	   	}
-	*/
+func TestPartOne(t *testing.T) {
+	vents := parseInput(inputString)
+	got := 1
+	want := 2
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
 
 func TestPartTwo(t *testing.T) {
