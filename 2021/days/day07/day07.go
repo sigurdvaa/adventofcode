b6c4ec29cdb226d8ea4e48ea2a8d7dea882f0266
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-08-08 22:10:23 +0200

    2021/07

--- 2021/days/day07/day07.go
+++ 2021/days/day07/day07.go
@@ -1,14 +1,15 @@
 package day07
 
 import (
-	//"aoc_2021/input"
+	"aoc_2021/input"
 	"fmt"
 )
 
 func Run() {
-	fmt.Println("Day XX: Example")
+	fmt.Println("Day 7: The Treachery of Whales")
 
-	//inputString := input.ReadDay("day07")
+	inputString := input.ReadDay("day07")
+	_ = inputString
 
 	fmt.Printf("Part One: TODO\n")
 	fmt.Printf("Part Two: TODO\n")
--- 2021/days/day07/day07_test.go
+++ 2021/days/day07/day07_test.go
@@ -4,15 +4,15 @@ import (
 	"testing"
 )
 
-func TestPartOne(t *testing.T) {
-	/*
-	   got := 1
-	   want := 2
+var inputString string = "16,1,2,0,4,2,7,1,2,14"
 
-	   	if got != want {
-	   		t.Errorf("got %d, wanted %d", got, want)
-	   	}
-	*/
+func TestPartOne(t *testing.T) {
+	got := 1
+	want := 37
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
 
 func TestPartTwo(t *testing.T) {
