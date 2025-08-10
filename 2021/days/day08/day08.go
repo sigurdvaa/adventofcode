92637894c18808ca20ebb7566b66d59f26b117e5
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2025-08-10 09:49:33 +0200

    2021/08

--- 2021/days/day08/day08.go
+++ 2021/days/day08/day08.go
@@ -3,18 +3,50 @@ package day08
 import (
 	"aoc_2021/input"
 	"fmt"
+	"slices"
+	"strings"
 )
 
-func parseInput(str string) {
+type display struct {
+	signals []string
+	output  []string
+}
 
+func parseInput(str string) []display {
+	displays := []display{}
+	for line := range strings.SplitSeq(str, "\n") {
+		if line == "" {
+			continue
+		}
+		split := strings.Split(line, " | ")
+		signals := strings.Split(split[0], " ")
+		output := strings.Split(split[1], " ")
+		displays = append(displays, display{signals, output})
+	}
+	return displays
+}
+
+func countEasyDigits(displays []display) int {
+	uniqueLen := []int{2, 3, 4, 7}
+	count := 0
+
+	for _, disp := range displays {
+		for _, digit := range disp.output {
+			if slices.Contains(uniqueLen, len(digit)) {
+				count += 1
+			}
+		}
+	}
+
+	return count
 }
 
 func Run() {
 	fmt.Println("Day 8: Seven Segment Search")
 
 	inputString := input.ReadDay("day08")
-	_ = inputString
+	displays := parseInput(inputString)
 
-	fmt.Printf("Part One: TODO\n")
+	fmt.Printf("Part One: %d\n", countEasyDigits(displays))
 	fmt.Printf("Part Two: TODO\n")
 }
