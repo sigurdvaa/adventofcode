a18d45075921eebf5d07f6b4f5c2a4a062d3b842
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-09-28 20:41:26 +0200

    2021/09

--- 2021/days/day09/day09.go
+++ 2021/days/day09/day09.go
@@ -27,8 +27,39 @@ func parseInput(str string) [][]int {
 	return heightMap
 }
 
+func lowPoint(heightMap [][]int, x int, y int) bool {
+	point := heightMap[y][x]
+
+	if y > 0 && heightMap[y-1][x] <= point {
+		return false
+	}
+
+	if y < len(heightMap)-1 && heightMap[y+1][x] <= point {
+		return false
+	}
+
+	if x > 0 && heightMap[y][x-1] <= point {
+		return false
+	}
+
+	if x < len(heightMap[y])-1 && heightMap[y][x+1] <= point {
+		return false
+	}
+
+	return true
+}
+
 func sumRiskLevelLowPoints(heightMap [][]int) int {
 	sum := 0
+
+	for y := range heightMap {
+		for x := range heightMap[y] {
+			if lowPoint(heightMap, x, y) {
+				sum += heightMap[y][x] + 1
+			}
+		}
+	}
+
 	return sum
 }
 
@@ -36,8 +67,8 @@ func Run() {
 	fmt.Println("Day 9: Smoke Basin")
 
 	inputString := input.ReadDay("day09")
-	_ = parseInput(inputString)
+	heightMap := parseInput(inputString)
 
-	fmt.Printf("Part One: TODO\n")
+	fmt.Printf("Part One: %d\n", sumRiskLevelLowPoints(heightMap))
 	fmt.Printf("Part Two: TODO\n")
 }
