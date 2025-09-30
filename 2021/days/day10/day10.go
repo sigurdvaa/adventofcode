6fc59b04cc3cfa2f53e787312a9b638aa613a5e6
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-09-30 20:39:59 +0200

    2021/10

--- 2021/days/day10/day10.go
+++ 2021/days/day10/day10.go
@@ -3,6 +3,7 @@ package day10
 import (
 	"aoc_2021/input"
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -17,12 +18,79 @@ func parseInput(str string) [][]rune {
 	return nav
 }
 
+func lineError(line []rune) (int, []rune) {
+	open := []rune{'(', '[', '{', '<'}
+	stack := []rune{}
+
+	for _, r := range line {
+		if slices.Contains(open, r) {
+			stack = append(stack, r)
+			continue
+		}
+
+		prev := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		switch r {
+		case ')':
+			if prev != '(' {
+				return 3, stack
+			}
+		case ']':
+			if prev != '[' {
+				return 57, stack
+			}
+		case '}':
+			if prev != '{' {
+				return 1197, stack
+			}
+		case '>':
+			if prev != '<' {
+				return 25137, stack
+			}
+		}
+	}
+
+	return 0, stack
+}
+
+func syntaxErrorScore(lines [][]rune) int {
+	score := 0
+
+	for _, line := range lines {
+		err, _ := lineError(line)
+		score += err
+	}
+
+	return score
+}
+
+func middleAutocompletionScore(lines [][]rune) int {
+	scores := []int{}
+	valueMap := map[rune]int{'(': 1, '[': 2, '{': 3, '<': 4}
+
+	for _, line := range lines {
+		err, stack := lineError(line)
+		if err == 0 {
+			score := 0
+			for i := len(stack) - 1; i >= 0; i-- {
+				score *= 5
+				score += valueMap[stack[i]]
+			}
+			scores = append(scores, score)
+		}
+	}
+
+	slices.Sort(scores)
+	return scores[len(scores)/2]
+}
+
 func Run() {
 	fmt.Println("Day 10: Syntax Scoring")
 
 	inputString := input.ReadDay("day10")
-	_ = inputString
+	lines := parseInput(inputString)
 
-	fmt.Printf("Part One: TODO\n")
-	fmt.Printf("Part Two: TODO\n")
+	fmt.Printf("Part One: %d\n", syntaxErrorScore(lines))
+	fmt.Printf("Part Two: %d\n", middleAutocompletionScore(lines))
 }
--- 2021/days/day10/day10_test.go
+++ 2021/days/day10/day10_test.go
@@ -16,9 +16,9 @@ var inputString string = `[({(<(())[]>[[{[]{<()<>>
 <{([{{}}[<[[[<>{}]]]>[]]`
 
 func TestPartOne(t *testing.T) {
-	_ = parseInput(inputString)
+	lines := parseInput(inputString)
 
-	got := 1
+	got := syntaxErrorScore(lines)
 	want := 26397
 
 	if got != want {
@@ -27,4 +27,12 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
+	lines := parseInput(inputString)
+
+	got := middleAutocompletionScore(lines)
+	want := 288957
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
