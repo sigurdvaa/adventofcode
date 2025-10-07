b808cd495029609d09971b71a62154b11e56101e
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-10-07 21:36:58 +0200

    2021/11

--- 2021/days/day11/day11.go
+++ 2021/days/day11/day11.go
@@ -1,15 +1,130 @@
 package day11
 
 import (
-	//"aoc_2021/input"
+	"aoc_2021/input"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
 
+func parseInput(str string) [][]int {
+	grid := [][]int{}
+
+	for line := range strings.SplitSeq(str, "\n") {
+		if line == "" {
+			continue
+		}
+		row := []int{}
+		for _, r := range line {
+			num, err := strconv.Atoi(string(r))
+			if err != nil {
+				log.Fatal(err)
+			}
+			row = append(row, num)
+		}
+		grid = append(grid, row)
+	}
+
+	return grid
+}
+
+func bumpOctopus(grid [][]int, x int, y int) int {
+	flashes := 0
+
+	if grid[y][x] != -1 {
+		grid[y][x] += 1
+		if grid[y][x] > 9 {
+			grid[y][x] = -1
+			flashes += 1
+
+			if y != 0 {
+				flashes += bumpOctopus(grid, x, y-1)
+				if x != 0 {
+					flashes += bumpOctopus(grid, x-1, y-1)
+				}
+				if x != len(grid[y])-1 {
+					flashes += bumpOctopus(grid, x+1, y-1)
+				}
+			}
+
+			if x != 0 {
+				flashes += bumpOctopus(grid, x-1, y)
+			}
+			if x != len(grid[y])-1 {
+				flashes += bumpOctopus(grid, x+1, y)
+			}
+
+			if y != len(grid)-1 {
+				flashes += bumpOctopus(grid, x, y+1)
+				if x != 0 {
+					flashes += bumpOctopus(grid, x-1, y+1)
+				}
+				if x != len(grid[y])-1 {
+					flashes += bumpOctopus(grid, x+1, y+1)
+				}
+			}
+		}
+	}
+
+	return flashes
+}
+
+func gameOfOctopus(grid [][]int, steps int) int {
+	flashes := 0
+
+	for i := 0; i < steps; i++ {
+		for y := range grid {
+			for x := range grid[y] {
+				flashes += bumpOctopus(grid, x, y)
+			}
+		}
+		for y := range grid {
+			for x := range grid[y] {
+				if grid[y][x] == -1 {
+					grid[y][x] = 0
+				}
+			}
+		}
+	}
+
+	return flashes
+}
+
+func gameOfOctopusFirstSync(grid [][]int) int {
+	total := len(grid) * len(grid[0])
+	steps := 0
+	for {
+		steps += 1
+		flashes := 0
+		for y := range grid {
+			for x := range grid[y] {
+				flashes += bumpOctopus(grid, x, y)
+			}
+		}
+
+		if flashes == total {
+			return steps
+		}
+
+		for y := range grid {
+			for x := range grid[y] {
+				if grid[y][x] == -1 {
+					grid[y][x] = 0
+				}
+			}
+		}
+	}
+}
+
 func Run() {
-	fmt.Println("Day XX: Example")
+	fmt.Println("Day 11: Dumbo Octopus")
 
-	//inputString := input.ReadDay("day11")
+	inputString := input.ReadDay("day11")
 
-	fmt.Printf("Part One: TODO\n")
-	fmt.Printf("Part Two: TODO\n")
+	grid := parseInput(inputString)
+	fmt.Printf("Part One: %d\n", gameOfOctopus(grid, 100))
+
+	grid = parseInput(inputString)
+	fmt.Printf("Part Two: %d\n", gameOfOctopusFirstSync(grid))
 }
--- 2021/days/day11/day11_test.go
+++ 2021/days/day11/day11_test.go
@@ -4,16 +4,33 @@ import (
 	"testing"
 )
 
-func TestPartOne(t *testing.T) {
-	/*
-	   got := 1
-	   want := 2
+var inputString string = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
 
-	   	if got != want {
-	   		t.Errorf("got %d, wanted %d", got, want)
-	   	}
-	*/
+func TestPartOne(t *testing.T) {
+	grid := parseInput(inputString)
+	got := gameOfOctopus(grid, 100)
+	want := 1656
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
 
 func TestPartTwo(t *testing.T) {
+	grid := parseInput(inputString)
+	got := gameOfOctopusFirstSync(grid)
+	want := 195
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
