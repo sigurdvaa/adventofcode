aeea879fcf6b6cd54e58549fd1bce1715fe9ab6c
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-10-10 18:55:03 +0200

    2021/12

--- 2021/days/day12/day12.go
+++ 2021/days/day12/day12.go
@@ -1,14 +1,44 @@
 package day12
 
 import (
-	//"aoc_2021/input"
+	"aoc_2021/input"
 	"fmt"
+	"strings"
 )
 
-func Run() {
-	fmt.Println("Day XX: Example")
+type CaveMap map[string][]string
 
-	//inputString := input.ReadDay("day12")
+func parseInput(str string) CaveMap {
+	var caveMap CaveMap = make(CaveMap)
+
+	for line := range strings.SplitSeq(str, "\n") {
+		if line == "" {
+			continue
+		}
+		split := strings.Split(line, "-")
+
+		_, ok := caveMap[split[0]]
+		if !ok {
+			caveMap[split[0]] = []string{}
+		}
+		_, ok = caveMap[split[1]]
+		if !ok {
+			caveMap[split[1]] = []string{}
+		}
+
+		caveMap[split[0]] = append(caveMap[split[0]], split[1])
+		caveMap[split[1]] = append(caveMap[split[1]], split[0])
+	}
+
+	return caveMap
+}
+
+func Run() {
+	fmt.Println("Day 12: Passage Pathing")
+
+	inputString := input.ReadDay("day12")
+	caveMap := parseInput(inputString)
+	_ = caveMap
 
 	fmt.Printf("Part One: TODO\n")
 	fmt.Printf("Part Two: TODO\n")
--- 2021/days/day12/day12_test.go
+++ 2021/days/day12/day12_test.go
@@ -4,15 +4,23 @@ import (
 	"testing"
 )
 
-func TestPartOne(t *testing.T) {
-	/*
-	   got := 1
-	   want := 2
+var inputString string = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`
 
-	   	if got != want {
-	   		t.Errorf("got %d, wanted %d", got, want)
-	   	}
-	*/
+func TestPartOne(t *testing.T) {
+	caveMap := parseInput(inputString)
+	_ = caveMap
+	got := 1
+	want := 2
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
 
 func TestPartTwo(t *testing.T) {
