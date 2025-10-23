d373e963ca5141365142fd4c7fcdb26ec2da44c2
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2025-10-23 19:44:37 +0200

    2021/16

--- 2021/days/day16/day16.go
+++ 2021/days/day16/day16.go
@@ -4,6 +4,7 @@ import (
 	"aoc_2021/input"
 	"fmt"
 	"log"
+	"math"
 	"strings"
 )
 
@@ -30,6 +31,14 @@ func parseInput(str string) []rune {
 	return []rune(strings.TrimSpace(str))
 }
 
+type Packet struct {
+	version int
+	typeid  int
+	size    int
+	value   int
+	sub     []Packet
+}
+
 type Stream struct {
 	data []rune
 	len  int
@@ -40,7 +49,7 @@ func (s *Stream) init(data []rune) {
 	s.data = data
 }
 
-func (s *Stream) read() {
+func (s *Stream) readByte() {
 	if len(s.data) > 0 {
 		s.buff = s.buff << 4
 		s.buff += hexMap[s.data[0]]
@@ -55,7 +64,7 @@ func (s *Stream) getValue(size int) int {
 	var value byte
 	for range size {
 		if s.len == 0 {
-			s.read()
+			s.readByte()
 		}
 		s.len -= 1
 		value <<= 1
@@ -64,23 +73,121 @@ func (s *Stream) getValue(size int) int {
 	return int(value)
 }
 
-func sumPacketVersion(packet []rune) int {
-	sum := 0
+func (s *Stream) getPacket() Packet {
+	packet := Packet{}
+	packet.version = s.getValue(3)
+	packet.typeid = s.getValue(3)
+	packet.size = 6
+
+	switch packet.typeid {
+	case 4: // literal
+		for {
+			tmp := s.getValue(5)
+			packet.size += 5
+			packet.value <<= 4
+			packet.value += tmp & 0b1111
+			if tmp&(1<<4) == 0 {
+				break
+			}
+		}
+	default: // operator
+		lengthid := s.getValue(1)
+		packet.size += 1
+
+		switch lengthid {
+		case 0:
+			subbits := s.getValue(15)
+			packet.size += 15
+			total := 0
+			for total < subbits {
+				subpacket := s.getPacket()
+				total += subpacket.size
+				packet.size += subpacket.size
+				packet.sub = append(packet.sub, subpacket)
+			}
+		case 1:
+			num := s.getValue(11)
+			packet.size += 11
+			for range num {
+				subpacket := s.getPacket()
+				packet.size += subpacket.size
+				packet.sub = append(packet.sub, subpacket)
+			}
+		}
+
+		switch packet.typeid {
+		case 0: // sum
+			for _, p := range packet.sub {
+				packet.value += p.value
+			}
+		case 1: // prod
+			packet.value = 1
+			for _, p := range packet.sub {
+				packet.value *= p.value
+			}
+		case 2: // min
+			packet.value = math.MaxInt
+			for _, p := range packet.sub {
+				if p.value < packet.value {
+					packet.value = p.value
+				}
+			}
+		case 3: // max
+			for _, p := range packet.sub {
+				if p.value > packet.value {
+					packet.value = p.value
+				}
+			}
+		case 5: // greater than
+			if packet.sub[0].value > packet.sub[1].value {
+				packet.value = 1
+			}
+		case 6: // less than
+			if packet.sub[0].value < packet.sub[1].value {
+				packet.value = 1
+			}
+		case 7: // equal
+			if packet.sub[0].value == packet.sub[1].value {
+				packet.value = 1
+			}
+		default:
+			log.Fatal("unreachable")
+		}
+
+	}
+
+	return packet
+}
+
+func recurSumVersions(packet Packet) int {
+	sum := packet.version
+	if len(packet.sub) == 0 {
+		return sum
+	}
+	for _, p := range packet.sub {
+		sum += recurSumVersions(p)
+	}
+	return sum
+}
+
+func sumPacketsVersion(packet []rune) int {
 	stream := &Stream{}
 	stream.init(packet)
+	return recurSumVersions(stream.getPacket())
+}
 
-	fmt.Println("version", stream.getValue(3))
-	fmt.Println("type", stream.getValue(3))
-
-	return sum
+func sumPacketsValue(str []rune) int {
+	stream := &Stream{}
+	stream.init(str)
+	return (stream.getPacket()).value
 }
 
 func Run() {
 	fmt.Println("Day 16: Packet Decoder")
 
 	inputString := input.ReadDay("day16")
-	packet := parseInput(inputString)
+	stream := parseInput(inputString)
 
-	fmt.Printf("Part One: %d\n", sumPacketVersion(packet))
-	fmt.Printf("Part Two: TODO\n")
+	fmt.Printf("Part One: %d\n", sumPacketsVersion(stream))
+	fmt.Printf("Part Two: %d\n", sumPacketsValue(stream))
 }
--- 2021/days/day16/day16_test.go
+++ 2021/days/day16/day16_test.go
@@ -10,6 +10,7 @@ func TestPartOne(t *testing.T) {
 		want  int
 	}{
 		{"D2FE28", 6},
+		{"38006F45291200", 9},
 		{"8A004A801A8002F478", 16},
 		{"620080001611562C8802118E34", 12},
 		{"C0015000016115A2E0802F182340", 23},
@@ -18,7 +19,7 @@ func TestPartOne(t *testing.T) {
 
 	for _, test := range tests {
 		packet := parseInput(test.input)
-		got := sumPacketVersion(packet)
+		got := sumPacketsVersion(packet)
 		want := test.want
 
 		if got != want {
@@ -28,4 +29,26 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{"C200B40A82", 3},
+		{"04005AC33890", 54},
+		{"880086C3E88112", 7},
+		{"CE00C43D881120", 9},
+		{"D8005AC2A8F0", 1},
+		{"F600BC2D8F", 0},
+		{"9C005AC2F8F0", 0},
+		{"9C0141080250320F1802104A08", 1},
+	}
+	for _, test := range tests {
+		packet := parseInput(test.input)
+		got := sumPacketsValue(packet)
+		want := test.want
+
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
+		}
+	}
 }
