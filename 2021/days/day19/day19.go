0950cbafefa306208d552ea1fdaebf6dec819740
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2025-10-25 19:20:46 +0200

    2021/19

--- 2021/days/day19/day19.go
+++ 2021/days/day19/day19.go
@@ -1,14 +1,34 @@
 package day19
 
 import (
-	//"aoc_2021/input"
+	"aoc_2021/input"
 	"fmt"
+	"strings"
 )
 
-func Run() {
-	fmt.Println("Day XX: Example")
+type Coord struct {
+	x int
+	y int
+	z int
+}
 
-	//inputString := input.ReadDay("day19")
+func parseInput(str string) [][]Coord {
+	coords := [][]Coord{}
+
+	for line := range strings.SplitSeq(str, "\n") {
+		if line == "" {
+			continue
+		}
+	}
+
+	return coords
+}
+
+func Run() {
+	fmt.Println("Day 19: Beacon Scanner")
+
+	inputString := input.ReadDay("day19")
+	_ = parseInput(inputString)
 
 	fmt.Printf("Part One: TODO\n")
 	fmt.Printf("Part Two: TODO\n")
