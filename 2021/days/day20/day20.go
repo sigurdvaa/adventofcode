fa8fccf55052ed54d2ec185082fe12de1524abcd
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2025-11-08 19:36:38 +0100

    2021/20

--- 2021/days/day20/day20.go
+++ 2021/days/day20/day20.go
@@ -24,17 +24,80 @@ func parseInput(str string) ([512]bool, [][]bool) {
 		if line == "" {
 			continue
 		}
-
 		if algo_parsed {
+			row := []bool{}
+			for _, r := range line {
+				row = append(row, parsePixel(r))
+			}
+			img = append(img, row)
 		} else {
 			for i, r := range line {
 				algo[i] = parsePixel(r)
 			}
 			algo_parsed = true
 		}
-
 	}
 	return algo, img
+}
+
+func calcPixelValue(img [][]bool, x int, y int) int {
+	val := 0
+	for iy := y - 1; iy < y+2; iy++ {
+		for ix := x - 1; ix < x+2; ix++ {
+			if iy >= 0 && iy < len(img) {
+				val <<= 1
+				if ix >= 0 && ix < len(img[iy]) {
+					if img[iy][ix] {
+						val += 1
+					}
+				}
+			}
+		}
+	}
+	return val
+}
+
+func enhanceImg(algo [512]bool, img [][]bool, steps int) [][]bool {
+	const PAD int = 1
+	for range steps {
+		nextY := len(img) + (PAD * 2)
+		nextX := len(img[0]) + (PAD * 2)
+		next := make([][]bool, nextY)
+		for y := range nextY {
+			next[y] = make([]bool, nextX)
+			for x := range nextX {
+				val := calcPixelValue(img, x-PAD, y-PAD)
+				next[y][x] = algo[val]
+			}
+		}
+		img = next
+	}
+	return img
+}
+
+func countLitPixels(img [][]bool) int {
+	count := 0
+	for _, row := range img {
+		for _, p := range row {
+			if p {
+				count += 1
+			}
+		}
+	}
+	return count
+}
+
+func printImg(img [][]bool) {
+	for _, row := range img {
+		for _, p := range row {
+			if p {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func Run() {
