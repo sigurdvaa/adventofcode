f328df2f9ecedd9c6b7ce497acb96417f134057a
Sigurd Våg Aaknes <sigurdvaa@protonmail.com>
2025-11-09 20:45:10 +0100

    2021/21

--- 2021/days/day21/day21.go
+++ 2021/days/day21/day21.go
@@ -1,14 +1,18 @@
 package day21
 
 import (
-	//"aoc_2021/input"
+	"aoc_2021/input"
 	"fmt"
 )
 
-func Run() {
-	fmt.Println("Day XX: Example")
+func parseInput(str string) (int, int) { return 0, 0 }
 
-	//inputString := input.ReadDay("day21")
+func Run() {
+	fmt.Println("Day 21: Dirac Dice")
+
+	inputString := input.ReadDay("day21")
+	p1, p2 := parseInput(inputString)
+	_, _ = p1, p2
 
 	fmt.Printf("Part One: TODO\n")
 	fmt.Printf("Part Two: TODO\n")
--- 2021/days/day21/day21_test.go
+++ 2021/days/day21/day21_test.go
@@ -4,15 +4,18 @@ import (
 	"testing"
 )
 
-func TestPartOne(t *testing.T) {
-	/*
-	   got := 1
-	   want := 2
+var inputString string = `Player 1 starting position: 4
+Player 2 starting position: 8`
 
-	   	if got != want {
-	   		t.Errorf("got %d, wanted %d", got, want)
-	   	}
-	*/
+func TestPartOne(t *testing.T) {
+	p1, p2 := parseInput(inputString)
+	_, _ = p1, p2
+	got := 1
+	want := 2
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
 
 func TestPartTwo(t *testing.T) {
