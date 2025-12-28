d8537cbd3d4593bec6cb61bc887c1b49a32ec110
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2025-12-28 15:43:54 +0100

    2021/22

--- 2021/days/day22/day22.go
+++ 2021/days/day22/day22.go
@@ -17,6 +17,39 @@ type Cuboid struct {
 	zEnd   int
 }
 
+func (c *Cuboid) copy() Cuboid {
+	return Cuboid{
+		c.value,
+		c.xStart, c.xEnd,
+		c.yStart, c.yEnd,
+		c.zStart, c.zEnd,
+	}
+}
+
+func (c *Cuboid) copySimple() Cuboid {
+	return Cuboid{
+		value:  c.value,
+		xStart: max(c.xStart, -50),
+		xEnd:   min(c.xEnd, 50),
+		yStart: max(c.yStart, -50),
+		yEnd:   min(c.yEnd, 50),
+		zStart: max(c.zStart, -50),
+		zEnd:   min(c.zEnd, 50),
+	}
+}
+
+func (c *Cuboid) size() int {
+	x := c.xEnd - c.xStart + 1
+	y := c.yEnd - c.yStart + 1
+	z := c.zEnd - c.zStart + 1
+
+	if x < 0 || y < 0 || z < 0 {
+		return 0
+	}
+
+	return x * y * z
+}
+
 type Coord struct {
 	x int
 	y int
@@ -58,47 +91,121 @@ func parseInput(str string) []Cuboid {
 	return procedure
 }
 
-func simplifyCuboid(c *Cuboid) {
-	c.xStart = max(c.xStart, -50)
-	c.xEnd = min(c.xEnd, 50)
-	c.yStart = max(c.yStart, -50)
-	c.yEnd = min(c.yEnd, 50)
-	c.zStart = max(c.zStart, -50)
-	c.zEnd = min(c.zEnd, 50)
+func overlappingRange(x1, x2, y1, y2 int) bool {
+	return (x1 >= y1 && x1 <= y2) ||
+		(x2 >= y1 && x2 <= y2) ||
+		(y1 >= x1 && y1 <= x2) ||
+		(y2 >= x1 && y2 <= x2) ||
+		(x1 < y1 && x2 > y2) ||
+		(y1 < x1 && y2 > x2)
 }
 
 func cuboidsIntersect(a Cuboid, b Cuboid) bool {
-	return a.xStart >= b.xStart && a.xStart <= b.xEnd &&
-		a.yStart >= b.yStart && a.yStart <= b.yEnd &&
-		a.zStart >= b.zStart && a.zStart <= b.zEnd
+	if !overlappingRange(a.xStart, a.xEnd, b.xStart, b.xEnd) {
+		return false
+	}
+	if !overlappingRange(a.yStart, a.yEnd, b.yStart, b.yEnd) {
+		return false
+	}
+	if !overlappingRange(a.zStart, a.zEnd, b.zStart, b.zEnd) {
+		return false
+	}
+	return true
+}
+
+func splitOuterCuboid(outer Cuboid, inner Cuboid) []Cuboid {
+	split := []Cuboid{}
+
+	//    _ _ _
+	// 1 |_|_|_|
+	// 3 |_|x|_| 4 // x: 5 front on z-axis, 6 behind on z-axis
+	// 2 |_|_|_| x
+	//	 y
+
+	// split 1
+	top := outer.copy()
+	top.yEnd = inner.yStart - 1
+	if top.size() > 0 {
+		split = append(split, top)
+	}
+
+	// split 2
+	bot := outer.copy()
+	bot.yStart = inner.yEnd + 1
+	if bot.size() > 0 {
+		split = append(split, bot)
+	}
+
+	// split 3
+	midLeft := outer.copy()
+	midLeft.xEnd = inner.xStart - 1
+	midLeft.yStart = max(outer.yStart, inner.yStart)
+	midLeft.yEnd = min(outer.yEnd, inner.yEnd)
+	if midLeft.size() > 0 {
+		split = append(split, midLeft)
+	}
+
+	// split 4
+	midRight := outer.copy()
+	midRight.xStart = inner.xEnd + 1
+	midRight.yStart = max(outer.yStart, inner.yStart)
+	midRight.yEnd = min(outer.yEnd, inner.yEnd)
+	if midRight.size() > 0 {
+		split = append(split, midRight)
+	}
+
+	// split 5
+	midFront := outer.copy()
+	midFront.xStart = max(outer.xStart, inner.xStart)
+	midFront.xEnd = min(outer.xEnd, inner.xEnd)
+	midFront.yStart = max(outer.yStart, inner.yStart)
+	midFront.yEnd = min(outer.yEnd, inner.yEnd)
+	midFront.zStart = outer.zStart
+	midFront.zEnd = inner.zStart - 1
+	if midFront.size() > 0 {
+		split = append(split, midFront)
+	}
+
+	// split 6
+	midBack := outer.copy()
+	midBack.xStart = max(outer.xStart, inner.xStart)
+	midBack.xEnd = min(outer.xEnd, inner.xEnd)
+	midBack.yStart = max(outer.yStart, inner.yStart)
+	midBack.yEnd = min(outer.yEnd, inner.yEnd)
+	midBack.zStart = inner.zEnd + 1
+	midBack.zEnd = outer.zEnd
+	if midBack.size() > 0 {
+		split = append(split, midBack)
+	}
+
+	return split
 }
 
 func runProcedure(procedure []Cuboid, simple bool) int {
 	cuboids := []Cuboid{}
 
 	for _, proc := range procedure {
-		nextCuboids := make([]Cuboid, 0, len(cuboids)*2)
-		noIntersects := true
-
-		// for each cuboids, check for intersections
-		for _, c := range cuboids {
-			if cuboidsIntersect(proc, c) {
-			}
-			splits := cuboidsIntersectSplit(proc, c)
-			if len(splits) > 0 {
-				noIntersects = false
-
-				// if "on", add, split cuboids on any intersects
-				if proc.value {
-					// intCuboid.value = proc.value
-					// nextCuboids = append(nextCuboids, intCuboid)
-				}
-
-				// split cuboid based in intersect and add all non-intersecting parts
+		if simple {
+			proc = proc.copySimple()
+			if proc.size() < 1 {
+				continue
 			}
 		}
 
-		if noIntersects && proc.value {
+		// overlap with existing cuboids are limited, and so will the growth be (~6 split, ~8 neighbors)
+		nextCuboids := make([]Cuboid, 0, len(cuboids)+(6*8))
+
+		// for each cuboids, check for intersect
+		for _, c := range cuboids {
+			if !cuboidsIntersect(c, proc) {
+				nextCuboids = append(nextCuboids, c)
+				continue
+			}
+			split := splitOuterCuboid(c, proc)
+			nextCuboids = append(nextCuboids, split...)
+		}
+
+		if proc.value {
 			nextCuboids = append(nextCuboids, proc)
 		}
 
@@ -107,9 +214,9 @@ func runProcedure(procedure []Cuboid, simple bool) int {
 
 	count := 0
 	for _, c := range cuboids {
-		count += (c.xEnd - c.xStart + 1) * (c.yEnd - c.yStart + 1) * (c.zEnd - c.zStart + 1)
+		count += c.size()
 	}
-	return 0
+	return count
 }
 
 func Run() {
@@ -117,8 +224,6 @@ func Run() {
 
 	inputString := input.ReadDay("day22")
 	procedure := parseInput(inputString)
-
 	fmt.Printf("Part One: %d\n", runProcedure(procedure, true))
-	// procedure = parseInput(inputString)
-	// fmt.Printf("Part Two: %d\n", runProcedure(procedure, false))
+	fmt.Printf("Part Two: %d\n", runProcedure(procedure, false))
 }
--- 2021/days/day22/day22_test.go
+++ 2021/days/day22/day22_test.go
@@ -4,40 +4,7 @@ import (
 	"testing"
 )
 
-func TestPartOne(t *testing.T) {
-	var inputString string = `on x=-20..26,y=-36..17,z=-47..7
-on x=-20..33,y=-21..23,z=-26..28
-on x=-22..28,y=-29..23,z=-38..16
-on x=-46..7,y=-6..46,z=-50..-1
-on x=-49..1,y=-3..46,z=-24..28
-on x=2..47,y=-22..22,z=-23..27
-on x=-27..23,y=-28..26,z=-21..29
-on x=-39..5,y=-6..47,z=-3..44
-on x=-30..21,y=-8..43,z=-13..34
-on x=-22..26,y=-27..20,z=-29..19
-off x=-48..-32,y=26..41,z=-47..-37
-on x=-12..35,y=6..50,z=-50..-2
-off x=-48..-32,y=-32..-16,z=-15..-5
-on x=-18..26,y=-33..15,z=-7..46
-off x=-40..-22,y=-38..-28,z=23..41
-on x=-16..35,y=-41..10,z=-47..6
-off x=-32..-23,y=11..30,z=-14..3
-on x=-49..-5,y=-3..45,z=-29..18
-off x=18..30,y=-20..-8,z=-3..13
-on x=-41..9,y=-7..43,z=-33..15
-on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
-on x=967..23432,y=45373..81175,z=27513..53682`
-	procedure := parseInput(inputString)
-	got := runProcedure(procedure, true)
-	want := 590784
-
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
-}
-
-func TestPartTwo(t *testing.T) {
-	var inputString string = `on x=-5..47,y=-31..22,z=-19..33
+var inputString string = `on x=-5..47,y=-31..22,z=-19..33
 on x=-44..5,y=-27..21,z=-14..35
 on x=-49..-1,y=-11..42,z=-10..38
 on x=-20..34,y=-40..6,z=-44..1
@@ -97,6 +64,18 @@ off x=-27365..46395,y=31009..98017,z=15428..76570
 off x=-70369..-16548,y=22648..78696,z=-1892..86821
 on x=-53470..21291,y=-120233..-33476,z=-44150..38147
 off x=-93533..-4276,y=-16170..68771,z=-104985..-24507`
+
+func TestPartOne(t *testing.T) {
+	procedure := parseInput(inputString)
+	got := runProcedure(procedure, true)
+	want := 474140
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
 	procedure := parseInput(inputString)
 	got := runProcedure(procedure, false)
 	want := 2758514936282235
